f62af314a7575bd0a2dd640fb3c4ba7abe42b8d6
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-03-15 10:31:20 +0100

    Current available samples

--- samples/python/cmgTuples_Data25ns_mAODv2_postProcessed.py
+++ samples/python/cmgTuples_Data25ns_mAODv2_postProcessed.py
@@ -1,39 +1,33 @@
 import copy, os, sys
-from StopsDilepton.tools.user import dataDir
-dir = dataDir
-
-DoubleEG_Run2015D = { "name" :"DoubleEG_Run2015D",
-"bins":[
-"DoubleEG_Run2015D_05Oct",
-"DoubleEG_Run2015D_v4"
-],
-"lumi":1000*(0.553+1.612),
-"texName":"DoubleEG (Run2015D)"
-}
-DoubleMuon_Run2015D = { "name" : "DoubleMuon_Run2015D",
-"bins":[
-"DoubleMuon_Run2015D_05Oct",
-"DoubleMuon_Run2015D_v4"
-],
-"lumi":1000*(0.551+1.613),
-"texName":"DoubleMuon (Run2015D)"
-}
-
-MuonEG_Run2015D = { "name" :"MuonEG_Run2015D",
-"bins":[
-"MuonEG_Run2015D_05Oct",
-"MuonEG_Run2015D_v4"
-],
-"lumi":1000*(0.528+1.598),
-"texName":"MuonEG (Run2015D)"
-}
-
-allSamples_Data25ns = [DoubleEG_Run2015D, MuonEG_Run2015D, DoubleMuon_Run2015D]
+from StopsDilepton.tools.user import data_output_directory
+from RootTools.core.Sample import Sample 
 import ROOT
-for s in allSamples_Data25ns:
-    s.update({
-        'dir' : dir,
-        'color':ROOT.kBlack,
-        'isData':True
-    })
 
+dirs = {}
+dirs['DoubleEG']       = ["DoubleEG_Run2015D_16Dec"]
+dirs['DoubleMuon']     = ["DoubleMuon_Run2015D_16Dec"]
+dirs['MuonEG']         = ["MuonEG_Run2015D_16Dec"]
+dirs['SingleElectron'] = ["SingleElectron_Run2015D_16Dec"]
+dirs['SingleMuon']     = ["SingleMuon_Run2015D_16Dec"]
+
+for key in dirs:
+  dirs[key] = [data_output_directory + '/postProcessed_Fall15_mAODv2/dilepTiny/' + dir for dir in dirs[key]]
+
+lumi = {}
+lumi['DoubleEG']       = 1000*(2.517)
+lumi['DoubleMuon']     = 1000*(2.517)
+lumi['MuonEG']         = 1000*(2.517)
+lumi['SingleElectron'] = 1000*(2.517)
+lumi['SingleMuon']     = 1000*(2.517)
+
+DoubleEG_Run2015D       = Sample.fromDirectory(name="DoubleEG_Run2015D",       treeName="Events", texName="DoubleEG (Run2015D)",       directory=dirs["DoubleEG"])
+#DoubleMuon_Run2015D     = Sample.fromDirectory(name="DoubleMuon_Run2015D",     treeName="Events", texName="DoubleMuon (Run2015D)",     directory=dirs["DoubleMuon"])
+#MuonEG_Run2015D         = Sample.fromDirectory(name="MuonEG_Run2015D",         treeName="Events", texName="MuonEG (Run2015D)",         directory=dirs["MuonEG"])
+#SingleElectron_Run2015D = Sample.fromDirectory(name="SingleElectron_Run2015D", treeName="Events", texName="SingleElectron (Run2015D)", directory=dirs["SingleElectron"])
+#SingleMuon_Run2015D     = Sample.fromDirectory(name="SingleMuon_Run2015D",     treeName="Events", texName="SingleMuon (Run2015D)",     directory=dirs["SingleMuon"])
+
+#allSamples_Data25ns = [DoubleEG_Run2015D, MuonEG_Run2015D, DoubleMuon_Run2015D, SingleElectron_Run2015D, SingleMuon_Run2015D]
+allSamples_Data25ns = [DoubleEG_Run2015D]
+for s in allSamples_Data25ns:
+  s.color   = ROOT.kBlack
+  s.isData  = True
