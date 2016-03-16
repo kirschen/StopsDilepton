07c3b33d172e8ea7c3b0862b2d4a4b1b2b3ba33f
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-03-16 19:26:25 +0100

    Add cmsPostProcessing script for Ghent

---
+++ cmgPostProcessing/doAllCmgPostProcessingGhent.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+mkdir -p log
+
+# Make sure cmgdataset cache is available
+if [ ! -d "/localgrid/$USER/.cmgdataset" ]; then
+  cp -r ~/.cmgdataset /localgrid/$USER
+fi
+
+# Run over samples given in input file
+while read -r sample; do
+    if [[ $sample = \#* || -z "$sample" ]] ; then
+      continue
+    fi
+    echo $sample
+    mkdir -p /localgrid/$USER/cmgPostProcessing/$sample
+    cp -r $CMSSW_BASE /localgrid/$USER/cmgPostProcessing/$sample
+    qsub -v sample=$sample,cmssw=$CMSSW_VERSION -q localgrid@cream02 -o log/$sample.txt -e log/$sample.txt runPostProcessingOnCream02.sh
+  done <$1
---
+++ cmgPostProcessing/runPostProcessingOnCream02.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "cmfPostProcessing of sample $sample in $cmssw"
+pwd=$PWD
+source $VO_CMS_SW_DIR/cmsset_default.sh
+cd /localgrid/$USER/cmgPostProcessing/$sample/$cmssw/src/StopsDilepton/cmgPostProcessing/
+eval `scram runtime -sh`
+python cmgPostProcessing.py --dataDir=/pnfs/iihe/cms/store/user/tomc/cmgTuples/763_4 --sample $sample --log=TRACE
+cd /localgrid/$USER
+rm -rf /localgrid/$USER/cmgPostProcessing/$sample
