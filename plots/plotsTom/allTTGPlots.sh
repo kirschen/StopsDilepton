a81910b8c50b98be9765980e69ec92b13222827a
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-03-30 18:14:59 +0200

    Comparing TTZ to TTGamma

--- plots/plotsTom/allTTGPlots.sh
+++ plots/plotsTom/allTTGPlots.sh
@@ -2,5 +2,5 @@
 mkdir -p log
 for mode in doubleMu doubleEle muEle
 do
-  qsub -v zMode=$zMode,mode=$mode -q localgrid@cream02 -o log/$zMode-$mode.log -e log/$zMode-$mode.log runTTGPlotsOnCream02.sh 
+  qsub -v mode=$mode -q localgrid@cream02 -o log/$mode.log -e log/$mode.log runTTGPlotsOnCream02.sh 
 done
