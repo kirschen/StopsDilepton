8ab13759c2a582dd01e9659a275b0fd30cb6fe09
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-04-05 12:00:03 +0200

    Change to singlelep

--- cmgPostProcessing/runPostProcessingOnCream02.sh
+++ cmgPostProcessing/runPostProcessingOnCream02.sh
@@ -1,10 +1,10 @@
 #PBS -l nodes=1:ppn=8
 #!/bin/bash
 echo "cmgPostProcessing of sample ${sample} in $cmssw"
-pwd=$PWD
+echo $TMPDIR
 source $VO_CMS_SW_DIR/cmsset_default.sh
-cd "/localgrid/$USER/cmgPostProcessing/${sample// /}/$cmssw/src/StopsDilepton/cmgPostProcessing/"
+cd "/localgrid/$USER/cmgPostProcessing/$skim/${sample// /}/$cmssw/src/StopsDilepton/cmgPostProcessing/"
 eval `scram runtime -sh`
-python cmgPostProcessing.py --dataDir=/pnfs/iihe/cms/store/user/tomc/cmgTuples/763_4 --sample ${sample} --log=TRACE --skim=dilep --keepPhotons --nJobs=8 --targetDir=/user/tomc/StopsDilepton/data
+python cmgPostProcessing.py --dataDir=/pnfs/iihe/cms/store/user/tomc/cmgTuples/763_4 --sample ${sample} --log=TRACE --skim=$skim --keepPhotons --minNJobs=8 --overwrite --targetDir=/user/tomc/StopsDilepton/data
 cd /localgrid/$USER
-rm -rf "/localgrid/$USER/cmgPostProcessing/${sample// /}"
+rm -rf "/localgrid/$USER/cmgPostProcessing/$skim/${sample// /}"
