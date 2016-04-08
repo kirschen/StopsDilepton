f7f2cfa6a98784c378541b68eeb7e8e54811a0a9
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-04-08 17:07:17 +0200

    Pass walltime option to batch script

--- cmgPostProcessing/doAllCmgPostProcessingGhent.sh
+++ cmgPostProcessing/doAllCmgPostProcessingGhent.sh
@@ -6,14 +6,18 @@ if [ ! -d "/localgrid/$USER/.cmgdataset" ]; then
   cp -r ~/.cmgdataset /localgrid/$USER
 fi
 
-skim="singlelepReallyTiny"
+skim="dilepTiny"
 # Run over samples given in input file
 while read -r sample; do
     if [[ ${sample} = \#* || -z "${sample}" ]] ; then
       continue
     fi
     echo "${sample}"
+    wallTime="02:00:00"
+    if [[ ${sample} = TTJets* ]]; then
+      wallTime="25:00:00"
+    fi
     mkdir -p "/localgrid/$USER/cmgPostProcessing/$skim/${sample// /}"
-    cp -r $CMSSW_BASE "/localgrid/$USER/cmgPostProcessing/$skim/${sample// /}"
-    qsub -v sample="${sample}",cmssw=$CMSSW_VERSION,skim=$skim -q localgrid@cream02 -o "log/${sample// /}.txt" -e "log/${sample// /}.txt" runPostProcessingOnCream02.sh
+    rsync -rq --exclude=.git --exclude=plots --exclude=crab_with_das --exclude=logs $CMSSW_BASE "/localgrid/$USER/cmgPostProcessing/$skim/${sample// /}"
+    qsub -v sample="${sample}",cmssw=$CMSSW_VERSION,skim=$skim -q localgrid@cream02 -o "log/${sample// /}.txt" -e "log/${sample// /}.txt" -l walltime=$wallTime runPostProcessingOnCream02.sh
   done <$1
--- cmgPostProcessing/runPostProcessingOnCream02.sh
+++ cmgPostProcessing/runPostProcessingOnCream02.sh
@@ -1,10 +1,13 @@
 #PBS -l nodes=1:ppn=8
-#!/bin/bash
+#!/bin/zsh
 echo "cmgPostProcessing of sample ${sample} in $cmssw"
-echo $TMPDIR
+echo "Id: $PBS_JOBID"
+
 source $VO_CMS_SW_DIR/cmsset_default.sh
 cd "/localgrid/$USER/cmgPostProcessing/$skim/${sample// /}/$cmssw/src/StopsDilepton/cmgPostProcessing/"
 eval `scram runtime -sh`
 python cmgPostProcessing.py --dataDir=/pnfs/iihe/cms/store/user/tomc/cmgTuples/763_4 --sample ${sample} --log=TRACE --skim=$skim --keepPhotons --minNJobs=8 --overwrite --targetDir=/user/tomc/StopsDilepton/data
 cd /localgrid/$USER
 rm -rf "/localgrid/$USER/cmgPostProcessing/$skim/${sample// /}"
+
+echo Done with walltime: $PBS_WALLTIME"
