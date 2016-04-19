8806eaadb3a094ae50c81fa8fe2e44d3bb0f5183
schoefbeck <robert.schoefbeck@cern.ch>
2016-04-20 00:04:21 +0200

    adding multiThread.py

---
+++ cmgPostProcessing/multiThread.py
@@ -0,0 +1,40 @@
+import subprocess
+import os
+import time
+import sys
+
+processes = set()
+max_processes = 5
+
+command = "echo"
+
+input_file = sys.argv[1]
+
+if os.path.exists(input_file):
+    with open(input_file) as f:
+        # Remove everything after #
+        jobs = [l.split('#')[0].rstrip() for l in f.readlines() ]
+        # remove empty lines
+        jobs = filter(lambda l:len(l)>0, jobs) 
+else:
+    raise ValueError( "Could not find file %s", input_file )
+
+if len(sys.argv):
+    extra_args = sys.argv[2:]
+else:
+    extra_args = []
+
+for job in jobs:
+    
+    cmds = job.split() + extra_args
+    print "Processing", cmds
+    processes.add(subprocess.Popen( cmds ))
+    if len(processes) >= max_processes:
+        os.wait()
+        processes.difference_update(
+            [p for p in processes if p.poll() is not None])
+
+#Check if all the child processes were closed
+for p in processes:
+    if p.poll() is None:
+        p.wait()
