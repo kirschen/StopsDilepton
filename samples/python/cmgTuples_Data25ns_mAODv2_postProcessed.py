71eaa046563ce29f24dd25ffc74047b01797362e
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-04-21 10:43:21 +0200

    Try to get postProcessing directory from main script and update lumi

--- samples/python/cmgTuples_Data25ns_mAODv2_postProcessed.py
+++ samples/python/cmgTuples_Data25ns_mAODv2_postProcessed.py
@@ -3,6 +3,13 @@ from StopsDilepton.tools.user import data_directory
 from RootTools.core.Sample import Sample 
 import ROOT
 
+# Take post processing directory if defined in main module
+try:
+  import sys
+  postProcessing_directory = sys.modules['__main__'].postProcessing_directory
+except:
+  postProcessing_directory = "postProcessed_Fall15_mAODv2/dilepTiny"
+
 dirs = {}
 dirs['DoubleEG']       = ["DoubleEG_Run2015D_16Dec"]
 dirs['DoubleMuon']     = ["DoubleMuon_Run2015D_16Dec"]
@@ -11,14 +18,7 @@ dirs['SingleElectron'] = ["SingleElectron_Run2015D_16Dec"]
 dirs['SingleMuon']     = ["SingleMuon_Run2015D_16Dec"]
 
 for key in dirs:
-  dirs[key] = [ os.path.join( data_directory, 'postProcessed_Fall15_mAODv2/dilepTiny', dir) for dir in dirs[key]]
-
-lumi = {}
-lumi['DoubleEG']       = 1000*(2.165)
-lumi['DoubleMuon']     = 1000*(2.165)
-lumi['MuonEG']         = 1000*(2.137)
-lumi['SingleElectron'] = 1000*(2.162)
-lumi['SingleMuon']     = 1000*(2.024)
+  dirs[key] = [ os.path.join( data_directory, postProcessing_directory, dir) for dir in dirs[key]]
 
 DoubleEG_Run2015D       = Sample.fromDirectory(name="DoubleEG_Run2015D",       treeName="Events", texName="DoubleEG (Run2015D)",       directory=dirs["DoubleEG"])
 DoubleMuon_Run2015D     = Sample.fromDirectory(name="DoubleMuon_Run2015D",     treeName="Events", texName="DoubleMuon (Run2015D)",     directory=dirs["DoubleMuon"])
@@ -27,10 +27,10 @@ SingleElectron_Run2015D = Sample.fromDirectory(name="SingleElectron_Run2015D", t
 SingleMuon_Run2015D     = Sample.fromDirectory(name="SingleMuon_Run2015D",     treeName="Events", texName="SingleMuon (Run2015D)",     directory=dirs["SingleMuon"])
 
 DoubleEG_Run2015D      .lumi =  1000*(2.165)
-DoubleMuon_Run2015D    .lumi =  1000*(2.136)
+DoubleMuon_Run2015D    .lumi =  1000*(2.165)
 MuonEG_Run2015D        .lumi =  1000*(2.137)
 SingleElectron_Run2015D.lumi =  1000*(2.165)
-SingleMuon_Run2015D    .lumi =  1000*(2.129)
+SingleMuon_Run2015D    .lumi =  1000*(2.165)
 
 allSamples_Data25ns = [DoubleEG_Run2015D, MuonEG_Run2015D, DoubleMuon_Run2015D, SingleElectron_Run2015D, SingleMuon_Run2015D]
 for s in allSamples_Data25ns:
--- samples/python/cmgTuples_Fall15_mAODv2_25ns_2l_postProcessed.py
+++ samples/python/cmgTuples_Fall15_mAODv2_25ns_2l_postProcessed.py
@@ -5,16 +5,23 @@ import ROOT
 from StopsDilepton.tools.user import data_directory
 from StopsDilepton.samples.color import color
 
+# Take post processing directory if defined in main module
+try:
+  import sys
+  postProcessing_directory = sys.modules['__main__'].postProcessing_directory
+except:
+  postProcessing_directory = "postProcessed_Fall15_mAODv2/dilepTiny"
+
 dirs = {}
 dirs['DY']               = ["DYJetsToLL_M10to50", "DYJetsToLL_M50"]
-dirs['DY_LO']            = ["DYJetsToLL_M5to50_LO", "DYJetsToLL_M50_LO"]
+#dirs['DY_LO']            = ["DYJetsToLL_M5to50_LO", "DYJetsToLL_M50_LO"]
 dirs['DY_HT_LO']         = ["DYJetsToLL_M50_LO_lheHT100", "DYJetsToLL_M50_HT100to200", "DYJetsToLL_M50_HT200to400", "DYJetsToLL_M50_HT400to600", "DYJetsToLL_M50_HT600toInf", "DYJetsToLL_M5to50_LO_lheHT200", "DYJetsToLL_M5to50_HT200to400", "DYJetsToLL_M5to50_HT400to600", "DYJetsToLL_M5to50_HT600toInf"]
 dirs['TTJets']           = ["TTJets_comb"]
 #dirs['TTJets_LO']        = ["TTJets_LO"]
-#dirs['TTLep_pow']        = ["TTLep_pow"]
-dirs['TTJets_Lep']       = ["TTJets_DiLepton_comb", "TTJets_SingleLeptonFromTbar_comb", "TTJets_SingleLeptonFromT_comb"]
+dirs['TTLep_pow']        = ["TTLep_pow"]
+#dirs['TTJets_Lep']       = ["TTJets_DiLepton_comb", "TTJets_SingleLeptonFromTbar_comb", "TTJets_SingleLeptonFromT_comb"]
 #dirs['TTJets_HT_LO']     = ["TTJets_LO_HT600to800_comb", "TTJets_LO_HT800to1200", "TTJets_LO_HT1200to2500_comb", "TTJets_LO_HT2500toInf"]
-dirs['singleTop']          = ["TBar_tWch", "TToLeptons_tch_amcatnlo_comb", "T_tWch"] # ["TBar_tWch_DS", "T_tWch_DS", "TToLeptons_sch"]
+dirs['singleTop']        = ["TBar_tWch", "TToLeptons_tch_amcatnlo_comb", "T_tWch"] # ["TBar_tWch_DS", "T_tWch_DS", "TToLeptons_sch"]
 dirs['TTX']              = ["TTHbb_comb", "TTHnobb_mWCutfix_ch0_comb", "TTWToLNu", "TTWToQQ",  "TTZToLLNuNu", "TTZToQQ", "tZq_ll", "tZq_nunu"]
 dirs['TTXNoZ']           = ["TTHbb_comb", "TTHnobb_mWCutfix_ch0_comb", "TTWToLNu", "TTWToQQ", "tZq_ll", "tZq_nunu"]
 dirs['TTH']              = ["TTHbb_comb", "TTHnobb_mWCutfix_ch0_comb"]
@@ -40,15 +47,15 @@ dirs['QCD_Mu5+EM+bcToE'] = ["QCD_Pt20to30_Mu5", "QCD_Pt50to80_Mu5", "QCD_Pt80to1
 #dirs['QCD_Mu5+EM+bcToE'] = ["QCD_Pt20to30_Mu5", "QCD_Pt50to80_Mu5", "QCD_Pt80to120_Mu5", "QCD_Pt120to170_Mu5", "QCD_Pt170to300_Mu5", "QCD_Pt300to470_Mu5", "QCD_Pt470to600_Mu5", "QCD_Pt600to800_Mu5", "QCD_Pt800to1000_Mu5", "QCD_Pt1000toInf_Mu5", "QCD_Pt_15to20_bcToE", "QCD_Pt_20to30_bcToE", "QCD_Pt_30to80_bcToE", "QCD_Pt_80to170_bcToE", "QCD_Pt_170to250_bcToE", "QCD_Pt_250toInf_bcToE", "QCD_Pt15to20_EMEnriched", "QCD_Pt20to30_EMEnriched", "QCD_Pt30to50_EMEnriched", "QCD_Pt50to80_EMEnriched", "QCD_Pt80to120_EMEnriched", "QCD_Pt120to170_EMEnriched", "QCD_Pt170to300_EMEnriched", "QCD_Pt300toInf_EMEnriched"]
 #dirs['QCD']              = ["QCD_Pt10to15", "QCD_Pt15to30", "QCD_Pt30to50", "QCD_Pt50to80", "QCD_Pt80to120", "QCD_Pt120to170", "QCD_Pt170to300", "QCD_Pt300to470", "QCD_Pt470to600", "QCD_Pt600to800", "QCD_Pt800to1000", "QCD_Pt1000to1400", "QCD_Pt1400to1800", "QCD_Pt1800to2400", "QCD_Pt2400to3200", "QCD_Pt3200"]
 
-directories = { key : [ os.path.join( data_directory, 'postProcessed_Fall15_mAODv2/dilepTiny', dir) for dir in dirs[key]] for key in dirs.keys()}
+directories = { key : [ os.path.join( data_directory, postProcessing_directory, dir) for dir in dirs[key]] for key in dirs.keys()}
 
 DY             = Sample.fromDirectory(name="DY",               treeName="Events", isData=False, color=color.DY,              texName="DY + Jets",                 directory=directories['DY'])
-DY_LO          = Sample.fromDirectory(name="DY_LO",            treeName="Events", isData=False, color=color.DY,              texName="DY + Jets (LO)",            directory=directories['DY_LO'])
+#DY_LO          = Sample.fromDirectory(name="DY_LO",            treeName="Events", isData=False, color=color.DY,              texName="DY + Jets (LO)",            directory=directories['DY_LO'])
 DY_HT_LO       = Sample.fromDirectory(name="DY_HT_LO",         treeName="Events", isData=False, color=color.DY,              texName="DY + Jets (LO,HT)",         directory=directories['DY_HT_LO'])
 TTJets         = Sample.fromDirectory(name="TTJets",           treeName="Events", isData=False, color=color.TTJets,              texName="t#bar{t} + Jets",           directory=directories['TTJets'])
 #TTJets_LO      = Sample.fromDirectory(name="TTJets_LO",        treeName="Events", isData=False, color=color.TTJets,              texName="t#bar{t} + Jets (LO)",      directory=directories['TTJets_LO'])
-#TTLep_pow      = Sample.fromDirectory(name="TTLep_pow",        treeName="Events", isData=False, color=color.TTJets,              texName="t#bar{t} + Jets (lep,pow)", directory=directories['TTLep_pow'])
-TTJets_Lep     = Sample.fromDirectory(name="TTJets_Lep",       treeName="Events", isData=False, color=color.TTJets,              texName="t#bar{t} + Jets (lep)",     directory=directories['TTJets_Lep'])
+TTLep_pow      = Sample.fromDirectory(name="TTLep_pow",        treeName="Events", isData=False, color=color.TTJets,              texName="t#bar{t} + Jets (lep,pow)", directory=directories['TTLep_pow'])
+#TTJets_Lep     = Sample.fromDirectory(name="TTJets_Lep",       treeName="Events", isData=False, color=color.TTJets,              texName="t#bar{t} + Jets (lep)",     directory=directories['TTJets_Lep'])
 #TTJets_HT_LO   = Sample.fromDirectory(name="TTJets_HT_LO",     treeName="Events", isData=False, color=color.TTJets,              texName="t#bar{t} + Jets (HT,LO)",   directory=directories['TTJets_HT_LO'])
 singleTop      = Sample.fromDirectory(name="singleTop",          treeName="Events", isData=False, color=color.singleTop,             texName="single top",                directory=directories['singleTop'])
 TTX            = Sample.fromDirectory(name="TTX",              treeName="Events", isData=False, color=color.TTX,     texName="t#bar{t}X",                 directory=directories['TTX'])
