f5ffbdf297abe0d5927ea0054859eea8b9b65197
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-04-27 11:45:32 +0200

    Update submission scripts for new T2 policy

--- cmgPostProcessing/runPostProcessingOnCream02.sh
+++ cmgPostProcessing/runPostProcessingOnCream02.sh
@@ -4,8 +4,8 @@ echo "cmgPostProcessing of sample ${sample} in $cmssw" >&2
 echo "Id: $PBS_JOBID" >&2
 
 source $VO_CMS_SW_DIR/cmsset_default.sh
-cd "/localgrid/$USER/cmgPostProcessing/$skim/${sample// /}/$cmssw/src/StopsDilepton/cmgPostProcessing/"
+cd "/user/$USER/cmgPostProcessing/$skim/${sample// /}/$cmssw/src/StopsDilepton/cmgPostProcessing/"
 eval `scram runtime -sh`
 python cmgPostProcessing.py --dataDir=/pnfs/iihe/cms/store/user/tomc/cmgTuples/763_1l_5 --sample ${sample} --log=TRACE --skim=$skim --keepPhotons --overwrite --targetDir=/user/tomc/StopsDilepton/data
-cd /localgrid/$USER
-rm -rf "/localgrid/$USER/cmgPostProcessing/$skim/${sample// /}"
+cd /user/$USER
+rm -rf "/user/$USER/cmgPostProcessing/$skim/${sample// /}"
