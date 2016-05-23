d4d140d413caafc650fe3d3460e270b01703e543
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-05-23 14:39:14 +0200

    Fix data-driven DY estimate

--- analysis/python/DataDrivenDYEstimate.py
+++ analysis/python/DataDrivenDYEstimate.py
@@ -1,16 +1,13 @@
-from StopsDilepton.tools.helpers import getYieldFromChain
 from math import sqrt
 from StopsDilepton.analysis.SystematicEstimator import SystematicEstimator
 from StopsDilepton.analysis.u_float import u_float
-#from StopsDilepton.tools.helpers import printHeader
 
 class DataDrivenDYEstimate(SystematicEstimator):
     def __init__(self, name, cacheDir=None):
         super(DataDrivenDYEstimate, self).__init__(name, cacheDir=cacheDir)
-#Concrete implementation of abstract method 'estimate' as defined in Systematic
-    def _estimate(self, region, channel, setup):
 
-#        printHeader("DD DY prediction for %s channel %s" %(self.name, channel))
+    #Concrete implementation of abstract method 'estimate' as defined in Systematic
+    def _estimate(self, region, channel, setup):
 
         #Sum of all channels for 'all'
         if channel=='all':
@@ -24,34 +21,34 @@ class DataDrivenDYEstimate(SystematicEstimator):
 
             if setup.verbose:
                 print "Using cut %s and weight %s"%(cut, weight)
-            return setup.lumi[channel]/1000. * u_float( getYieldFromChain(setup.sample['DY'][channel]['chain'], cutString = cut, weight=weight, returnError = True) )
+            return setup.lumi[channel]/1000. * u_float(**setup.sample['DY'][channel].getYieldFromDraw(selectionString = cut, weightString=weight))
 
         #Data driven for EE and MuMu
         else:
             preSelection = setup.preselection('MC', zWindow='offZ', channel=channel)
             weight = preSelection['weightStr']
 
-            assert abs(1.-setup.lumi[channel]/setup.sample['Data'][channel]['lumi'])<0.01, "Lumi specified in setup %f does not match lumi in data sample %f in channel %s"%(setup.lumi[channel], setup.sample['Data'][channel]['lumi'], channel)
-            cut_offZ_1b = "&&".join([region.cutString(setup.sys['selectionModifier']), setup.selection('MC', channel=channel, zWindow = 'offZ', **setup.defaultParameters(update={'nBTags':(1,-1)}))['cut'] ])
-            cut_onZ_1b  = "&&".join([region.cutString(setup.sys['selectionModifier']), setup.selection('MC', channel=channel, zWindow = 'onZ',  **setup.defaultParameters(update={'nBTags':(1,-1)}))['cut'] ])
-            cut_onZ_0b  = "&&".join([region.cutString(setup.sys['selectionModifier']), setup.selection('MC', channel=channel, zWindow = 'onZ',  **setup.defaultParameters(update={'nBTags':(0,0)}))['cut'] ])
-            cut_data_onZ_0b    = "&&".join([region.cutString(setup.sys['selectionModifier']), setup.selection('Data', channel=channel, zWindow = 'onZ',  **setup.defaultParameters(update={'nBTags':(0,0)}) )['cut'] ])
+            cut_offZ_1b     = "&&".join([region.cutString(setup.sys['selectionModifier']), setup.selection('MC',   channel=channel, zWindow = 'offZ', **setup.defaultParameters(update={'nBTags':(1,-1)}))['cut'] ])
+            cut_onZ_1b      = "&&".join([region.cutString(setup.sys['selectionModifier']), setup.selection('MC',   channel=channel, zWindow = 'onZ',  **setup.defaultParameters(update={'nBTags':(1,-1)}))['cut'] ])
+            cut_onZ_0b      = "&&".join([region.cutString(setup.sys['selectionModifier']), setup.selection('MC',   channel=channel, zWindow = 'onZ',  **setup.defaultParameters(update={'nBTags':(0,0)}))['cut'] ])
+            cut_data_onZ_0b = "&&".join([region.cutString(setup.sys['selectionModifier']), setup.selection('Data', channel=channel, zWindow = 'onZ',  **setup.defaultParameters(update={'nBTags':(0,0)}) )['cut'] ])
     #    R1 = DY-MC (offZ, 1b) / DY-MC (onZ, 1b)
     #    R2 = DY-MC (onZ, 1b) / DY-MC (onZ, 0b)
     #    DY-est = R1*R2*(Data(2l, onZ, 0b) - EWK(onZ, 0b)) = DY-MC (offZ, 1b) / DY-MC (onZ, 0b) *( Data(2l, onZ, 0b) - EWK(onZ, 0b))
 
-            yield_offZ_1b = u_float( getYieldFromChain(setup.sample['DY'][channel]['chain'], cutString = cut_offZ_1b, weight=weight, returnError = True))
-            if setup.verbose: print "yield_offZ_1b: %s"%yield_offZ_1b
-            yield_onZ_0b  = u_float( getYieldFromChain(setup.sample['DY'][channel]['chain'], cutString = cut_onZ_0b,  weight=weight, returnError = True))
-            if setup.verbose: print "yield_onZ_0b: %s"%yield_onZ_0b
-            yield_data    = u_float( getYieldFromChain(setup.sample['Data'][channel]['chain'], cutString = cut_data_onZ_0b,  weight=weight, returnError = True))
-            if setup.verbose: print "yield_data: %s (for cut: %s \n with weight: %s)"%(yield_data, cut_data_onZ_0b, weight)
+            yield_offZ_1b = u_float(**setup.sample['DY'][channel].getYieldFromDraw(  selectionString = cut_offZ_1b,     weightString=weight))
+            yield_onZ_0b  = u_float(**setup.sample['DY'][channel].getYieldFromDraw(  selectionString = cut_onZ_0b,      weightString=weight))
+            yield_data    = u_float(**setup.sample['Data'][channel].getYieldFromDraw(selectionString = cut_data_onZ_0b, weightString=weight))
+            if setup.verbose: 
+              print "yield_offZ_1b: %s"%yield_offZ_1b
+              print "yield_onZ_0b: %s"%yield_onZ_0b
+              print "yield_data: %s (for cut: %s \n with weight: %s)"%(yield_data, cut_data_onZ_0b, weight)
 
             #electroweak subtraction
             print "\n Substracting electroweak backgrounds from data: \n"
             yield_other = u_float(0., 0.)
             for s in ['TTJets' , 'TTZ' , 'other']:
-                yield_other+=u_float(getYieldFromChain(setup.sample[s][channel]['chain'], cutString = cut_onZ_0b,  weight=weight, returnError=True))
+                yield_other+=u_float(**setup.sample[s][channel].getYieldFromDraw(selectionString = cut_onZ_0b, weightString=weight))
                 if setup.verbose: print "yield_other_onZ_0b %s added, now: %s"%(s, yield_other)
 
             normRegYield = yield_data - yield_other
--- analysis/python/testDD-DY.py
+++ analysis/python/testDD-DY.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python
 from StopsDilepton.analysis.Region import Region
 from StopsDilepton.analysis.defaultAnalysis import setup
-
-#Always taking data lumi
-setup.lumi = {channel:setup.sample['Data'][channel]['lumi'] for channel in setup.sample['Data'].keys()}
-setup.loadChains()
-
+from StopsDilepton.samples.cmgTuples_Data25ns_mAODv2_postProcessed import *
 from StopsDilepton.analysis.DataDrivenDYEstimate import DataDrivenDYEstimate
+
+setup.dataSamples = {"EE" : DoubleEG_Run2015D, "MuMu" : DoubleMuon_Run2015D, "EMu" : MuonEG_Run2015D}
+setup.verbose = True
+
 estimateDY = DataDrivenDYEstimate(name='DY-DD', cacheDir=None)
 
-regionDY = Region('dl_mt2ll', (140,-1))
+regionDY = Region('dl_mt2ll', (0,-1))
 
-for channel in ['MuMu']:
+for channel, sample in setup.dataSamples.iteritems():
+    setup.lumi[channel] = sample.lumi
     res = estimateDY.cachedEstimate(regionDY,channel,setup)
     print "\n Result in ", channel," for estimate ", estimateDY.name, regionDY,":", res#, 'jer',jer, 'jec', jec
