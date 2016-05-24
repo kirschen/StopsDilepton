feeaa4116f1144a77c2e68ce972874554986f56a
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-05-24 10:26:32 +0200

    Fixing the layout a bit + adding plots for other region configurations

--- analysis/python/plot/region_plot.py
+++ analysis/python/plot/region_plot.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python
 #Standard imports
 import ROOT
+ROOT.gROOT.SetBatch(True)
+
 from math import sqrt, cos, sin, pi, acos
 import itertools
 
 #RootTools
 from RootTools.core.standard import *
 
-from StopsDilepton.analysis.mcAnalysis import setup, regions, bkgEstimators_detailed
+from StopsDilepton.analysis.mcAnalysis import setup, bkgEstimators_detailed
+from StopsDilepton.analysis.regions import defaultRegions, reducedRegionsA, reducedRegionsB, reducedRegionsAB
 import StopsDilepton.tools.user as user
 from StopsDilepton.samples.color import color
-#from StopsDilepton.samples.cmgTuples_FastSimT2tt_mAODv2_25ns_2l_postProcessed import *
 
 from StopsDilepton.analysis.MCBasedEstimate import MCBasedEstimate
 
@@ -18,27 +21,25 @@ from StopsDilepton.analysis.SetupHelpers import channels, allChannels
 # argParser
 import argparse
 argParser = argparse.ArgumentParser(description = "Argument parser")
-argParser.add_argument('--logLevel', 
-      action='store',
-      nargs='?',
-      choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET'],
-      default='INFO',
-      help="Log level for logging"
-)
+argParser.add_argument('--logLevel',       action='store', default='INFO',           nargs='?', choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET'],      help="Log level for logging")
+argParser.add_argument("--regions",        action='store', default='defaultRegions', nargs='?', choices=["defaultRegions","reducedRegionsA","reducedRegionsB","reducedRegionsAB"], help="which regions setup?")
+argParser.add_argument('--plot_directory', action='store', default='png25ns_3rdLep')
+args = argParser.parse_args()
 
-argParser.add_argument('--plot_directory',
-    default='png25ns_3rdLep',
-    action='store',
-)
 
-setup.analysis_results='/afs/hephy.at/data/rschoefbeck01/StopsDilepton/results/test6_noPU'
+if   args.regions == "defaultRegions":   regions = defaultRegions
+elif args.regions == "reducedRegionsA":  regions = reducedRegionsA
+elif args.regions == "reducedRegionsB":  regions = reducedRegionsB
+elif args.regions == "reducedRegionsAB": regions = reducedRegionsAB
+else: raise Exception("Unknown regions setup")
+
 signalSetup = setup.sysClone(parameters={'useTriggers':False})
 
 for estimator in bkgEstimators_detailed:
     estimator.style = styles.fillStyle( getattr( color, estimator.name ) )
 
-from StopsDilepton.samples.cmgTuples_FastSimT2tt_mAODv2_25ns_2l_postProcessed import *
-signalEstimators = [ MCBasedEstimate(name=s.name,    sample={channel:s for channel in allChannels}, cacheDir=setup.defaultCacheDir() ) for s in [T2tt_450_0] ]
+from StopsDilepton.samples.cmgTuples_FastSimT2tt_mAODv2_25ns_postProcessed import *
+signalEstimators = [ MCBasedEstimate(name=s.name,  sample={channel:s for channel in allChannels}, cacheDir=setup.defaultCacheDir() ) for s in [T2tt_450_0] ]
 for estimator in signalEstimators:
     estimator.style = styles.lineStyle( getattr( color, estimator.name ) )
  
@@ -46,7 +47,6 @@ estimators = bkgEstimators_detailed + signalEstimators
 for e in estimators:
     e.initCache(setup.defaultCacheDir())
 
-args = argParser.parse_args()
 
 # Logging
 import StopsDilepton.tools.logger as logger
@@ -56,7 +56,7 @@ logger_rt = logger_rt.get_logger(args.logLevel, logFile = None )
 
 def getRegionHisto(estimate, regions, channel, setup):
 
-    h = ROOT.TH1F(estimate.name, estimate.name, len(regions), 0, len(regions))
+    h = ROOT.TH1F(estimate.name + channel, estimate.name, len(regions), 0, len(regions))
     h.legendText = estimate.name 
     for i, r in enumerate(regions):
         res = estimate.cachedEstimate(r, channel, setup, save=False)
@@ -68,15 +68,18 @@ def getRegionHisto(estimate, regions, channel, setup):
 def drawObjects( regions ):
     tex = ROOT.TLatex()
 #    tex.SetNDC()
-    tex.SetTextSize(0.02)
+    tex.SetTextSize(0.015)
     tex.SetTextAngle(90)
-    tex.SetTextAlign(11) # align right
-    lines = [(i+1-0.3, 10**4, r.texString()) for i, r in enumerate(regions)]
+    tex.SetTextAlign(12) # align right
+#    lines = [(i+1-0.3, 10**4, r.texString()) for i, r in enumerate(regions)]
+    lines =  [(i+1-0.3, 10**-6.5,  r.texStringForVar('dl_mt2ll'))   for i, r in enumerate(regions)]
+    lines += [(i+1-0.3, 10**-5.5,  r.texStringForVar('dl_mt2blbl')) for i, r in enumerate(regions)]
+    lines += [(i+1-0.3, 10**-4.5,  r.texStringForVar('dl_mt2bb'))   for i, r in enumerate(regions)]
     return [tex.DrawLatex(*l) for l in lines] 
 
 for channel in allChannels:
 
-    regions_ = regions
+    regions_ = regions[1:]
     # regions_ = filter(lambda r: r.vals['dl_mt2ll'][0]>0, regions)
 
     bkg_histos = [ getRegionHisto(e, regions=regions_, channel=channel, setup = setup) for e in bkgEstimators_detailed ]
@@ -84,11 +87,12 @@ for channel in allChannels:
 
     region_plot = Plot.fromHisto(name = channel+"_bkgs", histos = [ bkg_histos ] + sig_histos, texX = "SR number", texY = "Events" )
     plotting.draw( region_plot, \
-        plot_directory = user.plot_directory+'/etc/', 
+        plot_directory = os.path.join(user.plot_directory, args.regions), 
         logX = False, logY = True, 
         # sorting = True , 
-        yRange = (10**-3, 10**12), 
+        yRange = (10**-3, 10**4), 
         widths = {'x_width':1000, 'y_width':2000},
         drawObjects = drawObjects(regions_), 
-        legend = (0.7,0.93-0.025*(len(bkg_histos) + len(sig_histos)), 0.95, 0.93)
+        legend = (0.7,0.93-0.025*(len(bkg_histos) + len(sig_histos)), 0.95, 0.93),
+        canvasModifications = [lambda c: c.SetWindowSize(c.GetWw(), int(c.GetWh()*1.5)), lambda c : c.GetPad(0).SetBottomMargin(0.33)] # Keep some space for the labels
     )
