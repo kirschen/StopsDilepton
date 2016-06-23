b6b1c7e0a07b1fa702451534c60f9c6f4c260e0c
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-06-23 13:39:53 +0200

    Updates to the table

--- analysis/python/run/makeSystematicsTable.py
+++ analysis/python/run/makeSystematicsTable.py
@@ -4,11 +4,9 @@ import argparse
 argParser = argparse.ArgumentParser(description = "Argument parser")
 argParser.add_argument("--metSigMin",      action='store', default=5,                   type=int,                                                                                                         help="metSigMin?")
 argParser.add_argument("--metMin",         action='store', default=80,                  type=int,                                                                                                         help="metMin?")
-argParser.add_argument("--estimateDY",     action='store', default='DY',                nargs='?', choices=["DY","DY-DD"],                                                                                help="which DY estimate?")
-argParser.add_argument("--estimateTTZ",    action='store', default='TTZ',               nargs='?', choices=["TTZ","TTZ-DD","TTZ-DD-Top16009"],                                                            help="which TTZ estimate?")
-argParser.add_argument("--estimateTTJets", action='store', default='TTJets',            nargs='?', choices=["TTZJets","TTJets-DD"],                                                                       help="which TTJets estimate?")
 argParser.add_argument("--regions",        action='store', default='reducedRegionsNew', nargs='?', choices=["defaultRegions","superRegion","superRegion140"],                                             help="which regions setup?")
 argParser.add_argument("--signal",         action='store', default='T2tt',              nargs='?', choices=["T2tt","DM"],                                                                                 help="which signal?")
+argParser.add_argument("--estimates",      action='store', default='mc',                nargs='?', choices=["mc","dd"],                                                                                   help="mc estimators or data-driven estimators?")
 args = argParser.parse_args()
 
 from StopsDilepton.analysis.SetupHelpers import allChannels
@@ -36,7 +34,9 @@ elif args.regions == "superRegion":       regions = superRegion
 elif args.regions == "superRegion140":    regions = superRegion140
 else: raise Exception("Unknown regions setup")
 
-estimators = constructEstimatorList([args.estimateTTJets, 'other-detailed', args.estimateDY, args.estimateTTZ])
+if   args.estimates == "mc": estimators = constructEstimatorList(["TTJets","TTZ","DY", 'other-detailed'])
+elif args.estimates == "dd": estimators = constructEstimatorList(["TTJets","TTJets-DD","TTZ","TTZ-DD","TTZ-DD-Top16009","DY","DY-DD"])
+
 for e in estimators:
     e.initCache(setup.defaultCacheDir())
 
@@ -46,7 +46,7 @@ from StopsDilepton.analysis.u_float                                           im
 from math                                                                     import sqrt
 
 
-texdir      = os.path.join(setup.analysis_results, setup.prefix(), 'tables', args.signal, args.regions)
+texdir      = os.path.join(setup.analysis_results, setup.prefix(), 'tables' + ("_dd" if args.estimates == "dd" else ""), args.signal, args.regions)
 
 try:
   os.makedirs(texdir)
@@ -55,8 +55,8 @@ except:
 
 def displaySysValue(val):
   roundedVal = int(100*val+0.99)/100.    # round to next 0.0x
-  if roundedVal <= 0: return "-"
-  return str(roundedVal)
+  if roundedVal <= 0.: return "-"
+  return "%0.2f" % roundedVal
 
 
 # Make a separate table for each of regions
@@ -78,19 +78,32 @@ for channel in ["EE","MuMu","EMu","all"]:
       
       # One row for each estimator
       for e in estimators:
-	f.write("  " + e.name)
+        try:
+          name = e.sample[channel].texName
+        except:
+          try:
+            texNames = [e.sample[c].texName for c in ['MuMu','EMu','EE']]		# If all, only take texName if it is the same for all channels
+	    if texNames.count(texNames[0]) == len(texNames):
+	      name = texNames[0]
+            else:
+	      name = e.name
+          except:
+	    name = e.name
+        name = name.replace('#','\\') # Make it tex format
+
+	f.write(" $" + name + "$ ")
 	expected = int(100*e.cachedEstimate(       r, channel, setup).val+0.5)/100.
 	f.write(" & " + str(expected))
-        if expected <= 0:
-          f.write(" & -"*(len(columns)-1))
-        else:
-	  f.write(" & " + displaySysValue(e.cachedEstimate(       r, channel, setup).sigma))
-	  f.write(" & " + displaySysValue(e.PUSystematic(         r, channel, setup).val))
-	  f.write(" & " + displaySysValue(e.JECSystematic(        r, channel, setup).val))
-	  f.write(" & " + displaySysValue(e.JERSystematic(        r, channel, setup).val))
-	  f.write(" & " + displaySysValue(e.topPtSystematic(      r, channel, setup).val))
-	  f.write(" & " + displaySysValue(e.btaggingSFbSystematic(r, channel, setup).val))
-	  f.write(" & " + displaySysValue(e.btaggingSFlSystematic(r, channel, setup).val)) 
+#        if expected <= 0:
+#          f.write(" & -"*(len(columns)-1))
+#        else:
+	f.write(" & " + displaySysValue(e.cachedEstimate(       r, channel, setup).sigma))
+	f.write(" & " + displaySysValue(e.PUSystematic(         r, channel, setup).val))
+	f.write(" & " + displaySysValue(e.JECSystematic(        r, channel, setup).val))
+	f.write(" & " + displaySysValue(e.JERSystematic(        r, channel, setup).val))
+	f.write(" & " + displaySysValue(e.topPtSystematic(      r, channel, setup).val))
+	f.write(" & " + displaySysValue(e.btaggingSFbSystematic(r, channel, setup).val))
+	f.write(" & " + displaySysValue(e.btaggingSFlSystematic(r, channel, setup).val)) 
 	f.write(" \\\\ \n")
       f.write("\\end{tabular} \n")
       f.write("\\caption{Yields and uncertainties for each background in the signal region $" + r.texString(useRootLatex = False) + "$ in channel " + channel + "} \n")
