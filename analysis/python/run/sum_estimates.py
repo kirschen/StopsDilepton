03b89ba0b8ecbd60163c1d6db0b810d313659d65
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-07-01 00:15:46 +0200

    move to 80X

--- analysis/python/run/sum_estimates.py
+++ analysis/python/run/sum_estimates.py
@@ -6,7 +6,7 @@ parser.add_option('--logLevel',              dest="logLevel",              defau
 
 from StopsDilepton.analysis.SetupHelpers import allChannels
 from StopsDilepton.analysis.estimators import setup, constructEstimatorList
-from StopsDilepton.analysis.regions import reducedRegionsNew, superRegion, superRegion140
+from StopsDilepton.analysis.regions import regions80X, superRegion, superRegion140
 from StopsDilepton.analysis.Cache import Cache
 import os
 
@@ -20,15 +20,15 @@ logger_rt = logger_rt.get_logger(options.logLevel, logFile = None )
 # These are the ones we are going to sum
 estimators = constructEstimatorList(["TTJets","TTZ","DY", 'other-detailed'])
 
-regions = set(reducedRegionsNew + superRegion + superRegion140)
+regions = set(regions80X + superRegion + superRegion140)
 
 for e in estimators:
     e.initCache(setup.defaultCacheDir())
 
 sumCache = Cache(os.path.join(setup.defaultCacheDir(), 'sum.pkl'), verbosity=2)
 
-for c in ["EE","MuMu","EMu","all"]:
+for c in allChannels:
   for r in regions:
-    for r, c, s in estimators[0].getBkgSysJobs(r, c, setup):
+    for r, c, s in estimators[0].getBkgSysJobs(r, c, setup) + [(r, c, setup)]:
       res = sum(e.cachedEstimate(r, c, s, save=False) for e in estimators)
       sumCache.add(estimators[0].uniqueKey(r, c, s), res, save=True)
