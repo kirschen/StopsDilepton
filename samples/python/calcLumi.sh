f3c9e6687a8b17cdbcf31ba3c37fc5a22b59fbd3
schoefbeck <robert.schoefbeck@cern.ch>
2016-07-28 17:02:21 +0200

    Run2016BCD

--- samples/python/calcLumi.sh
+++ samples/python/calcLumi.sh
@@ -1,4 +1,4 @@
 #for x in `ls /afs/hephy.at/data/rschoefbeck02/cmgTuples/postProcessed_80X_v2/dilep/*/*.json`; do echo $x; brilcalc lumi -b "STABLE BEAMS" -i $x -u /fb; done
 #for x in `ls /afs/hephy.at/data/rschoefbeck02/cmgTuples/postProcessed_80X_v2/dilep/*/*.json`; do echo $x; brilcalc lumi -i $x -u /fb; done
-for x in `ls /afs/hephy.at/data/rschoefbeck02/cmgTuples/postProcessed_80X_v11/dilepTiny/*/*.json`; do echo $x; brilcalc lumi -b "STABLE BEAMS" --normtag=/afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json -i $x -u /fb; done
+for x in `ls /afs/hephy.at/data/rschoefbeck02/cmgTuples/postProcessed_80X_v12/dilepTiny/*/*.json`; do echo $x; brilcalc lumi -b "STABLE BEAMS" --normtag=/afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json -i $x -u /fb; done
 
