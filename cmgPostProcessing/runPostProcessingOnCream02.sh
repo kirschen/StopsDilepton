ae8f94d975f0d6731c3cffe7114cc9329958f849
UAEDF-tomc <tom.cornelis@ua.ac.be>
2016-08-30 14:45:40 +0200

    postprocessing on T2_BE_IIHE

--- cmgPostProcessing/runPostProcessingOnCream02.sh
+++ cmgPostProcessing/runPostProcessingOnCream02.sh
@@ -6,6 +6,6 @@ echo "Id: $PBS_JOBID" >&2
 source $VO_CMS_SW_DIR/cmsset_default.sh
 cd "/user/$USER/cmgPostProcessing/$skim/${sample// /}/$cmssw/src/StopsDilepton/cmgPostProcessing/"
 eval `scram runtime -sh`
-python cmgPostProcessing.py --dataDir=/pnfs/iihe/cms/store/user/tomc/cmgTuples/763_1l_5 --sample ${sample} --log=TRACE --skim=$skim --keepPhotons --overwrite --targetDir=/user/tomc/StopsDilepton/data
+python cmgPostProcessing.py --dataDir=/pnfs/iihe/cms/store/user/tomc/cmgTuples/80X_v6 --sample ${sample} --log=TRACE --skim=$skim --overwrite --processingEra=postProcessed_80X_tom --targetDir=/user/tomc/StopsDilepton/data
 cd /user/$USER
 rm -rf "/user/$USER/cmgPostProcessing/$skim/${sample// /}"
