0080e25784ac2390baa73c09928acf9e9d3ffe98
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-01-03 21:16:48 +0100

    user.py updated

--- tools/python/user.py
+++ tools/python/user.py
@@ -47,8 +47,8 @@ if os.environ['USER'] in ['tomc']:
     # Where you store the data for plotting, analysis etc.
     data_directory             = "/user/tomc/StopsDilepton/data"
     # Where the plots go
-    plot_directory             = "/user/tomc/StopsDilepton/plots_80X_v23"
+    plot_directory             = "/user/tomc/StopsDilepton/plots_80X_v24"
     # Analysis result files
-    analysis_results           = "/user/tomc/StopsDilepton/results_80X_v23"
+    analysis_results           = "/user/tomc/StopsDilepton/results_80X_v24"
     # Release for limit calculation (seems the 74X release is now also recommended for users)
     combineReleaseLocation     = '/user/tomc/StopsDilepton/CMSSW_7_4_7/src/'
