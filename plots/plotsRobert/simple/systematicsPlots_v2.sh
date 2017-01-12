44d4597810ad8584d80331dcdcbf164ecc607f16
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2017-01-12 23:48:49 +0100

    cutInterpreter bugfix and sys

--- plots/plotsRobert/simple/systematicsPlots_v2.sh
+++ plots/plotsRobert/simple/systematicsPlots_v2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#python systematicsPlots_v2.py $@ --normalizationSelection njet2p-btag1p-relIso0.12-looseLeptonVeto-mll20-met80-metSig5-dPhiJet0-dPhiJet1-mt2llTo100 --selection njet2p-btag1p-relIso0.12-looseLeptonVeto-mll20-met80-metSig5-dPhiJet0-dPhiJet1 #default
-#python systematicsPlots_v2.py $@ --normalizationSelection njet2p-btag1p-relIso0.12-looseLeptonVeto-mll20-met80-metSig5-dPhiJet0-dPhiJet1-mt2llTo100 --selection njet2p-btag1p-relIso0.12-looseLeptonVeto-mll20-met80-metSig5-dPhiJet0-dPhiJet1-mt2ll100 #high mt2ll
+python systematicsPlots_v2.py $@ --normalizationSelection njet2p-btag1p-relIso0.12-looseLeptonVeto-mll20-met80-metSig5-dPhiJet0-dPhiJet1-mt2llTo100 --selection njet2p-btag1p-relIso0.12-looseLeptonVeto-mll20-met80-metSig5-dPhiJet0-dPhiJet1 #default
+python systematicsPlots_v2.py $@ --normalizationSelection njet2p-btag1p-relIso0.12-looseLeptonVeto-mll20-met80-metSig5-dPhiJet0-dPhiJet1-mt2llTo100 --selection njet2p-btag1p-relIso0.12-looseLeptonVeto-mll20-met80-metSig5-dPhiJet0-dPhiJet1-mt2ll100 #high mt2ll
 
 python systematicsPlots_v2.py $@ --normalizationSelection njet01-btag0-relIso0.12-looseLeptonVeto-mll20-met80-dPhiJet0-dPhiJet1-mt2llTo100 --selection njet01-btag0-relIso0.12-looseLeptonVeto-mll20-met80-dPhiJet0-dPhiJet1 #a
 python systematicsPlots_v2.py $@ --normalizationSelection njet01-btag1p-relIso0.12-looseLeptonVeto-mll20-met80-dPhiJet0-dPhiJet1-mt2llTo100 --selection njet01-btag1p-relIso0.12-looseLeptonVeto-mll20-met80-dPhiJet0-dPhiJet1 #b
