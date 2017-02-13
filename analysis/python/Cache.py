08319e0653411434f5599e83266d473ef13b931e
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-02-13 10:17:17 +0100

    fix tabs/spaces

--- analysis/python/Cache.py
+++ analysis/python/Cache.py
@@ -10,35 +10,35 @@ class Cache:
     def initCache(self, filename):
         self.filename=filename
         if not os.path.isfile(filename) or os.stat(self.filename).st_size == 0:  # Check if there's already something in there
-	  if self.verbosity>=1: print "File %s not found. Starting new cache."%filename
-	  self._cache = {}
+          if self.verbosity>=1: print "File %s not found. Starting new cache."%filename
+          self._cache = {}
         else:
-	  try:
+          try:
             waitForLock(filename)
-	    with open(filename, 'r') as f:
-	      self._cache = pickle.load(f)
+            with open(filename, 'r') as f:
+              self._cache = pickle.load(f)
             removeLock(filename)
-	    if self.verbosity>=1: print "Loaded cache file %s"%filename
-	  except:# (IOError, ValueError, EOFError):
+            if self.verbosity>=1: print "Loaded cache file %s"%filename
+          except:# (IOError, ValueError, EOFError):
               print "File %s looks corrupted, please check before proceeding" % filename
-	      exit(1)
+              exit(1)
 
     # Try to reload to cache file in order to get updates from other jobs/threads
     def reload(self, removeKey=None):
         if not os.path.isfile(self.filename) or os.stat(self.filename).st_size == 0:
           pass
         else:
-	  try:
-	    temp = self._cache
+          try:
+            temp = self._cache
             waitForLock(self.filename)
-	    with open(self.filename, 'r') as f:
-	      self._cache = pickle.load(f)
-	      if removeKey and removeKey in self._cache:   # This is to avoid that an old value overwrites an updated value when using parallel jobs
-		del self._cache[removeKey]
-	      self._cache.update(temp)
+            with open(self.filename, 'r') as f:
+              self._cache = pickle.load(f)
+              if removeKey and removeKey in self._cache:   # This is to avoid that an old value overwrites an updated value when using parallel jobs
+                del self._cache[removeKey]
+              self._cache.update(temp)
             removeLock(self.filename)
           except Exception as e:# (IOError, ValueError, EOFError):
-	      if self.verbosity>=1: print "Cache file %s could not be reloaded"%self.filename
+              if self.verbosity>=1: print "Cache file %s could not be reloaded"%self.filename
               removeLock(self.filename)
 
     def contains (self, key):
@@ -56,8 +56,8 @@ class Cache:
 
     def save(self, removeKey = None):
         self.reload(removeKey)
-	waitForLock(self.filename)
-	with open(self.filename, 'w') as f:
-	  pickle.dump(self._cache, f)
+        waitForLock(self.filename)
+        with open(self.filename, 'w') as f:
+          pickle.dump(self._cache, f)
         removeLock(self.filename)
         if self.verbosity>=2: print "Written cache file %s"%self.filename
