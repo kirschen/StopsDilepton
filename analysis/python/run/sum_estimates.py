47883044a3fa2a11167b06d423e96cc332e4700c
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-03-03 12:49:15 +0100

    updating SF for the summed estimate

--- analysis/python/run/sum_estimates.py
+++ analysis/python/run/sum_estimates.py
@@ -25,8 +25,9 @@ for e in estimators:
 sumCache = Cache(os.path.join(setup.defaultCacheDir(), 'sum.pkl'), verbosity=2)
 
 def applySF(val, e):
-      if   e.name.count('DY'):         return val*1.115
-      elif e.name.count('multiBoson'): return val*1.265
+      if   e.name.count('DY'):         return val*1.31
+      elif e.name.count('multiBoson'): return val*1.19
+      elif e.name.count('TTZ'):        return val*1.09
       else:                            return val
 
 def wrapper(config):
