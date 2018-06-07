fa073ad06e1fd29de98fe883e384ea896131a54d
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2018-06-07 11:54:05 +0200

    preliminary private nanoAOD production

---
+++ nanoAOD/crabConfig.py
@@ -0,0 +1,45 @@
+import os
+from CRABClient.UserUtilities import getUsernameFromSiteDB
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+# Set variables
+production_label = os.environ["CRAB_PROD_LABEL"]
+dataset=os.environ["CRAB_DATASET"]
+unitsPerJob = int(os.environ["CRAB_UNITS_PER_JOB"])
+if "CRAB_TOTAL_UNITS" in os.environ: totalUnits = os.environ["CRAB_TOTAL_UNITS"]
+
+config.section_("General")
+config.General.transferLogs = True
+config.General.requestName = production_label
+config.General.workArea = config.General.requestName
+
+config.section_("JobType")
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'nanoAOD_cfg.py'
+config.JobType.outputFiles = ['nanoAOD.root']
+
+config.section_("Data")
+config.Data.inputDataset = dataset
+
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+if "IS_DATA" in os.environ:
+    config.Data.lumiMask = 'json/Cert_314472-316723_13TeV_PromptReco_Collisions18_JSON.txt'
+config.Data.ignoreLocality = False
+
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+config.Data.unitsPerJob = unitsPerJob#10
+if "CRAB_TOTAL_UNITS" in os.environ: config.Data.totalUnits = int(totalUnits)#8
+
+config.section_("Site")
+#config.Site.blacklist = ['T2_US_Purdue', 'T2_US_Nebraska', 'T2_US_MIT', 'T2_US_Caltech']
+config.Site.storageSite = 'T2_AT_Vienna'
+
+config.section_("User")
+
+config.section_("Debug")
+
+
---
+++ nanoAOD/launch.py
@@ -0,0 +1,41 @@
+import imp, os, sys
+from optparse import OptionParser
+import re
+
+
+parser = OptionParser(usage="python launch.py [options] component1 [ component2 ...]", \
+                          description="Launch heppy jobs with CRAB3. Components correspond to the variables defined in heppy_samples.py (their name attributes)")
+parser.add_option("--production_label", dest="production_label", help="production label", default="heppy")
+parser.add_option("--remoteDir", dest="remoteDir", help="remote subdirectory", default="")
+parser.add_option("--unitsPerJob", dest="unitsPerJob", help="Nr. of units (files) / crab job", type="int", default=1)
+parser.add_option("--totalUnits", dest="totalUnits", help="Total nr. of units (files)", type="int", default=None)
+parser.add_option("--dataset", dest="dataset", help="DAS dataset", default="/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM")
+parser.add_option("--isData", action='store_true', dest="isData", help="lumi mask (for data)", default=False)
+( options, args ) = parser.parse_args()
+
+
+os.system("scram runtime -sh")
+os.system("source /cvmfs/cms.cern.ch/crab3/crab.sh")
+
+#os.environ["CMG_REMOTE_DIR"]  = options.remoteDir
+os.environ["CRAB_UNITS_PER_JOB"] = str(options.unitsPerJob)
+if options.totalUnits:
+    os.environ["CRAB_TOTAL_UNITS"] = str(options.totalUnits)
+
+m=re.match("\/(.*)\/(.*)\/(.*)",options.dataset)
+#print m.group(1), m.group(2)
+if options.isData:
+    os.environ["CRAB_PROD_LABEL"]  = options.production_label+"_"+m.group(1)+"_"+m.group(2)
+else:
+    os.environ["CRAB_PROD_LABEL"]  = options.production_label+"_"+m.group(1)#+"_"+m.group(2)
+
+if options.isData:
+    os.environ["IS_DATA"] = "True"
+
+#print os.environ["CRAB_PROD_LABEL"]
+
+os.environ["CRAB_DATASET"] = options.dataset
+
+os.system("which crab")
+os.system("crab submit -c crabConfig.py")
+
