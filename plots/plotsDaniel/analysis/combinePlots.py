5336fa265d2c7702c2889874e2b37f5a93c7a253
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2019-04-25 17:04:47 +0200

    first attempt to combine plots for all 3 years

---
+++ plots/plotsDaniel/analysis/combinePlots.py
@@ -0,0 +1,94 @@
+import ROOT, os
+ROOT.gROOT.SetBatch(True)
+import itertools
+
+from math                                import sqrt, cos, sin, pi, atan2
+from RootTools.core.standard             import *
+from StopsDilepton.tools.user            import plot_directory, analysis_results
+from Samples.Tools.metFilters            import getFilterCut
+from StopsDilepton.tools.cutInterpreter  import cutInterpreter
+from StopsDilepton.samples.color import color
+
+import pickle
+import copy
+
+processes = ['TTZ','TTXNoZ','DY','Top_pow','multiBoson']
+
+
+hists = {}
+
+files = ['/afs/hephy.at/user/d/dspitzbart/www/stopsDileptonLegacy/analysisPlots/2016/v0p7/mumumu_log/trilepTight-lepSelTight-njet3p-btag1p-onZ1/Z1_pt.root', '/afs/hephy.at/user/d/dspitzbart/www/stopsDileptonLegacy/analysisPlots/2017/v0p7/mumumu_log/trilepTight-lepSelTight-njet3p-btag1p-onZ1/Z1_pt.root', '/afs/hephy.at/user/d/dspitzbart/www/stopsDileptonLegacy/analysisPlots/2018/v0p7/mumumu_log/trilepTight-lepSelTight-njet3p-btag1p-onZ1/Z1_pt.root']
+
+for i,f in enumerate(files):
+    tmp_f = ROOT.TFile(f, 'READ')
+    tmp_c = tmp_f.Get(tmp_f.GetListOfKeys()[0].GetName())
+    tmp_hists = tmp_c.GetListOfPrimitives()[0].GetListOfPrimitives()
+    
+    hists[i] = {proc:{} for proc in processes + ['data']}
+    
+    hierarchy = []
+    
+    for k in range(len(tmp_hists)):
+        for proc in processes + ['data']:
+            if proc.lower() in tmp_hists[k].GetName().lower():
+                if not proc in hierarchy:
+                    hists[i][proc] = copy.deepcopy(tmp_hists[k])
+                    if not proc == 'data':
+                        hierarchy += [proc]
+    tmp_f.Close()
+
+    for j,proc in enumerate(hierarchy):
+        if j < len(hierarchy)-1:
+            hists[i][proc].Add(hists[i][hierarchy[j+1]], -1)
+
+for i, f in enumerate(files):
+    if i>0:
+        for proc in processes + ['data']:
+            hists[0][proc].Add(hists[i][proc])
+    else:
+        for proc in processes:
+            hists[0][proc].style = styles.fillStyle(getattr(color, proc))
+            hists[0][proc].legendText = proc.replace('_HT_LO','').replace('_pow','').replace('4l','(4l)').replace('NoZ','(no Z)')
+        
+        hists[0]['data'].style = styles.errorStyle(color=ROOT.kBlack)
+        hists[0]['data'].legendText = "Data"
+
+def drawObjects( isData=False, lumi=36. ):
+    tex = ROOT.TLatex()
+    tex.SetNDC()
+    tex.SetTextSize(0.04)
+    tex.SetTextAlign(11) # align right
+    lines = [
+      (0.15, 0.95, 'CMS Simulation') if not isData else (0.15, 0.95, 'CMS Preliminary'),
+      (0.70, 0.95, '%sfb^{-1} (13 TeV)'%int(lumi) )
+    ]
+    return [tex.DrawLatex(*l) for l in lines]
+
+
+dO = drawObjects( isData=True, lumi=round(35.9+41.5+60.,0))
+
+plots_CR = [ [hists[0][proc] for proc in processes], [hists[0]['data'] ] ]
+
+plotting.draw(
+    Plot.fromHisto("Z1_pt_3l",
+                plots_CR,
+                texX = "p_{T}(Z candidate) (GeV)",
+                texY = "Events"
+            ),
+    plot_directory = os.path.join(plot_directory, "propaganda"),
+    logX = False, logY = True,
+    sorting = True,
+    yRange = (0.03, "auto"),
+    scaling = {0:1},
+    legend = (0.50,0.88-0.04*sum(map(len, plots_CR)),0.9,0.88), 
+#if not args.noData else (0.50,0.9-0.047*sum(map(len, plots_CR)),0.85,0.9)
+#    legend = [ (0.19,0.9-0.03*sum(map(len, plots_CR)),0.9,0.9), 2],
+#    widths = {'x_width':700, 'y_width':600},
+    ratio = {'yRange': (0.1, 1.9), 'texY':"Data/Pred."},
+    drawObjects = dO,
+    copyIndexPHP = True,
+)
+
+
+
+
