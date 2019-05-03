1ac7a04d4137a41bb868eec6980be39f6e8aa98a
Markus Doppler <markusdoppler@me.com>
2019-05-03 07:39:04 +0200

    updated lepton plots

--- plots/plotsMarkus/analysis/leptonPlots.sh
+++ plots/plotsMarkus/analysis/leptonPlots.sh
@@ -1,6 +1,24 @@
-python leptonPlots.py --era Run2018 --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ
-python leptonPlots.py --era Run2018 --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll90To150
-python leptonPlots.py --era Run2018 --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll100
-python leptonPlots.py --era Run2018 --study Electron --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ
-python leptonPlots.py --era Run2018 --study Electron --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll90To150
-python leptonPlots.py --era Run2018 --study Electron --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll100
+# muon study plots
+#python leptonPlots.py --era Run2018ABC --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ
+#python leptonPlots.py --era Run2018ABC --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll90To150
+#python leptonPlots.py --era Run2018ABC --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll100
+python leptonPlots.py --era Run2018D --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ
+#python leptonPlots.py --era Run2018D --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll90To150
+#python leptonPlots.py --era Run2018D --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll100
+#python leptonPlots.py --era Run2018 --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ
+#python leptonPlots.py --era Run2018 --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll90To150
+#python leptonPlots.py --era Run2018 --study Muon --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll100
+
+#python leptonPlots.py --era Run2018ABC --minmax --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ
+#python leptonPlots.py --era Run2018ABC --minmax --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll90To150
+#python leptonPlots.py --era Run2018ABC --minmax --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll100
+#python leptonPlots.py --era Run2018D --minmax --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ
+#python leptonPlots.py --era Run2018D --minmax --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll90To150
+#python leptonPlots.py --era Run2018D --minmax --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll100
+#python leptonPlots.py --era Run2018 --minmax --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ
+#python leptonPlots.py --era Run2018 --minmax --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll90To150
+#python leptonPlots.py --era Run2018 --minmax --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll100
+
+#python leptonPlots.py --era Run2018 --study Electron --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ
+#python leptonPlots.py --era Run2018 --study Electron --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll90To150
+#python leptonPlots.py --era Run2018 --study Electron --selection lepSel-POGMetSig12-njet2p-btag0-relIso0.12-looseLeptonVeto-mll20-dPhiJet0-dPhiJet1-onZ-mt2ll100
