eb39ad1521f7683cd7dc5a88f815f090defee529
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2019-05-29 14:59:32 +0200

    speeding up the scan

--- analysis/python/run/runLimitOnHephyBatch.py
+++ analysis/python/run/runLimitOnHephyBatch.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 import os
-from StopsDilepton.samples.cmgTuples_FastSimT2tt_mAODv2_25ns_postProcessed import signals_T2tt
-from StopsDilepton.samples.cmgTuples_FastSimT2bX_mAODv2_25ns_postProcessed import signals_T2bt, signals_T2bW
 
-from StopsDilepton.samples.cmgTuples_FastSimT8bbllnunu_mAODv2_25ns_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p05, signals_T8bbllnunu_XCha0p5_XSlep0p09, signals_T8bbllnunu_XCha0p5_XSlep0p5, signals_T8bbllnunu_XCha0p5_XSlep0p95
-from StopsDilepton.samples.cmgTuples_FullSimTTbarDM_mAODv2_25ns_postProcessed import signals_TTbarDM
+data_directory              = '/afs/hephy.at/data/dspitzbart03/nanoTuples/'
+postProcessing_directory    = 'stops_2017_nano_v0p7/dilep/'
+from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T2tt
 
-#signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p05]
-signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p09]
-#signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p5]
-#signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p95]
-#signalEstimators = [s.name for s in signals_T2tt]
-#signalEstimators = [s.name for s in signals_T2bt]
-#signalEstimators = [s.name for s in signals_T2bW]
-#signalEstimators = [s.name for s in signals_TTbarDM]
+
+signalEstimators = [s.name for s in signals_T2tt]
 
 import time
 
@@ -21,17 +14,5 @@ import time
 cmd = "echo"
 
 for i, estimator in enumerate(signalEstimators):
-  #if "750_100" in estimator:
-    #print "###"*10# == "T2tt_800_100": print "#"*100
-    #print i, estimator
-  os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p09 --fitAll --keepCard          --only=%s'"%str(i))
-#  os.system(cmd+" 'python run_limit.py --signal T2tt --fitAll            --only=%s'"%str(i))
-#  os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p5--controlDYVV --only=%s'"%str(i))
-#  os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p5--controlTTZ  --only=%s'"%str(i))
-#  os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p5--fitAll      --only=%s'"%str(i))
-#  time.sleep(5)
-
-# 4,5,21,22 for popping
-#for i in range(3):
-#  os.system('python run_limit.py --signal TTbarDM --fitAll --only 22 --aggregate --popFromSR=%s'%str(i))
+    os.system(cmd+" 'python run_limit.py --signal T2tt --expected --year 2017 --skipFitDiagnostics --only=%s'"%str(i))
 
