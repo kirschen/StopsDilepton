c96f35d1ee9f0999ea8f4c64febd988dea797a4d
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2019-07-01 06:57:14 +0200

    schoef

--- postProcessing/nanoPostProcessing_Run2017.sh
+++ postProcessing/nanoPostProcessing_Run2017.sh
@@ -1,29 +1,29 @@
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample DoubleMuon_Run2017B_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample DoubleMuon_Run2017C_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample DoubleMuon_Run2017D_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample DoubleMuon_Run2017E_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample DoubleMuon_Run2017F_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample DoubleMuon_Run2017B_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample DoubleMuon_Run2017C_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample DoubleMuon_Run2017D_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample DoubleMuon_Run2017E_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample DoubleMuon_Run2017F_31Mar2018 #SPLIT10
 #
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample DoubleEG_Run2017B_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample DoubleEG_Run2017C_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample DoubleEG_Run2017D_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample DoubleEG_Run2017E_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample DoubleEG_Run2017F_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample DoubleEG_Run2017B_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample DoubleEG_Run2017C_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample DoubleEG_Run2017D_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample DoubleEG_Run2017E_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample DoubleEG_Run2017F_31Mar2018 #SPLIT10
 #
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample MuonEG_Run2017B_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample MuonEG_Run2017C_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample MuonEG_Run2017D_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample MuonEG_Run2017E_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample MuonEG_Run2017F_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample MuonEG_Run2017B_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample MuonEG_Run2017C_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample MuonEG_Run2017D_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample MuonEG_Run2017E_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample MuonEG_Run2017F_31Mar2018 #SPLIT10
 #
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample SingleMuon_Run2017B_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample SingleMuon_Run2017C_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample SingleMuon_Run2017D_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample SingleMuon_Run2017E_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample SingleMuon_Run2017F_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample SingleMuon_Run2017B_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample SingleMuon_Run2017C_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample SingleMuon_Run2017D_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample SingleMuon_Run2017E_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample SingleMuon_Run2017F_31Mar2018 #SPLIT10
 #
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample SingleElectron_Run2017B_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample SingleElectron_Run2017C_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample SingleElectron_Run2017D_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample SingleElectron_Run2017E_31Mar2018 #SPLIT10
-python nanoPostProcessing.py  --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p13  --sample SingleElectron_Run2017F_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample SingleElectron_Run2017B_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample SingleElectron_Run2017C_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample SingleElectron_Run2017D_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample SingleElectron_Run2017E_31Mar2018 #SPLIT10
+python nanoPostProcessing.py  --targetDir /afs/hephy.at/data/cms06/nanoTuples/ --forceProxy --skim dilep --triggerSelection --year 2017 --processingEra stops_2017_nano_v0p15  --sample SingleElectron_Run2017F_31Mar2018 #SPLIT10
