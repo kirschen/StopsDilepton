89c38ac20d98bccf3683556806bfa41926aec376
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2019-08-02 14:30:43 +0200

    signal splitting shell file

---
+++ postProcessing/nanoSignalSplitting.sh
@@ -0,0 +1,22 @@
+#python nanoSignalSplitting.py --year 2017 --fastSim --T2tt          --processingEra stops_2017_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_150to250
+#python nanoSignalSplitting.py --year 2017 --fastSim --T2tt          --processingEra stops_2017_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_250to350
+#python nanoSignalSplitting.py --year 2017 --fastSim --T2tt          --processingEra stops_2017_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_350to400
+#python nanoSignalSplitting.py --year 2017 --fastSim --T2tt          --processingEra stops_2017_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_400to1200 # SPLIT6
+#
+#python nanoSignalSplitting.py --year 2018 --fastSim --T2tt          --processingEra stops_2018_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_150to250
+#python nanoSignalSplitting.py --year 2018 --fastSim --T2tt          --processingEra stops_2018_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_250to350
+#python nanoSignalSplitting.py --year 2018 --fastSim --T2tt          --processingEra stops_2018_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_350to400
+#python nanoSignalSplitting.py --year 2018 --fastSim --T2tt          --processingEra stops_2018_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_400to1200 # SPLIT6
+#
+python nanoSignalSplitting.py --year 2016 --fastSim --T2tt          --processingEra stops_2016_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_150to250
+#python nanoSignalSplitting.py --year 2016 --fastSim --T2tt          --processingEra stops_2016_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_250to350
+python nanoSignalSplitting.py --year 2016 --fastSim --T2tt          --processingEra stops_2016_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_350to400
+python nanoSignalSplitting.py --year 2016 --fastSim --T2tt          --processingEra stops_2016_nano_v0p13 --skim dilep --samples SMS_T2tt_mStop_400to1200 # SPLIT8
+
+#python nanoSignalSplitting.py --year 2016 --fastSim --T8bbllnunu    --processingEra stops_2016_nano_v4 --skim dilep --samples SMS_T8bbllnunu_XCha0p5_XSlep0p05 # SPLIT40
+#python nanoSignalSplitting.py --year 2016 --fastSim --T8bbllnunu    --processingEra stops_2016_nano_v4 --skim dilep --samples SMS_T8bbllnunu_XCha0p5_XSlep0p5 # SPLIT40
+#python nanoSignalSplitting.py --year 2016 --fastSim --T8bbllnunu    --processingEra stops_2016_nano_v4 --skim dilep --samples SMS_T8bbllnunu_XCha0p5_XSlep0p5_mN1_700_1000 # SPLIT40
+#python nanoSignalSplitting.py --year 2016 --fastSim --T8bbllnunu    --processingEra stops_2016_nano_v4 --skim dilep --samples SMS_T8bbllnunu_XCha0p5_XSlep0p95 # SPLIT20
+#python nanoSignalSplitting.py --year 2016 --fastSim --T8bbllnunu    --processingEra stops_2016_nano_v4 --skim dilep --samples SMS_T8bbllnunu_XCha0p5_XSlep0p95_mN1_700_1300 # SPLIT10
+#
+#python nanoSignalSplitting.py --year 2017 --fastSim --T2tt          --processingEra stops_2017_nano_v0p7 --skim dilep --samples SMS_T2tt_mStop_400to1200 # SPLIT8
