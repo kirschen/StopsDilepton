3585ace82df7782f396f0b97c448012f3de79c08
Markus Doppler <markusdoppler@me.com>
2019-09-13 14:11:04 +0200

    skipping T8bbllnunu_XCha0p5_XSlep0p05_1052_0 for now

--- analysis/python/run/runLimitOnHephyBatch.py
+++ analysis/python/run/runLimitOnHephyBatch.py
@@ -3,11 +3,11 @@ import os
 
 data_directory              = '/afs/hephy.at/data/cms05/nanoTuples/'
 postProcessing_directory    = 'stops_2016_nano_v0p16/dilep/'
-#from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T2tt
-#from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T2bW
-#from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p05 
-#from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p5  
-#from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p95 
+from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T2tt
+from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T2bW
+from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p05 
+from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p5  
+from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p95 
 
 #data_directory              = '/afs/hephy.at/data/cms05/nanoTuples/'
 #postProcessing_directory    = 'stops_2017_nano_v0p16/dilep/'
@@ -16,20 +16,21 @@ postProcessing_directory    = 'stops_2016_nano_v0p16/dilep/'
 #from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p05 
 #from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p5  
 #from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p95 
-#
-data_directory              = '/afs/hephy.at/data/cms05/nanoTuples/'
-postProcessing_directory    = 'stops_2018_nano_v0p16/dilep/'
-from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T2tt
-from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T2bW
-from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p05
-from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p5 
-from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p95
+
+#data_directory              = '/afs/hephy.at/data/cms05/nanoTuples/'
+#postProcessing_directory    = 'stops_2018_nano_v0p16/dilep/'
+#from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T2tt
+#from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T2bW
+#from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p05
+#from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p5 
+#from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p95
+
 
 #signalEstimators = [s.name for s in signals_T2tt]
 #signalEstimators = [s.name for s in signals_T2bW]
-signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p05]
+#signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p05]
 #signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p5]
-#signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p95]
+signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p95]
 
 import time
 
@@ -43,7 +44,7 @@ for i, estimator in enumerate(signalEstimators):
     # fitAll
     #os.system(cmd+" 'python run_limit.py --signal T2tt --expected --fitAll  --year 2018 --skipFitDiagnostics --only=%s'"%str(i))
     #os.system(cmd+" 'python run_limit.py --signal T2bW --expected --fitAll  --year 2018 --skipFitDiagnostics --only=%s'"%str(i))
-    os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p05 --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
+    #os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p05 --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
     #os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p5 --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
-    #os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p95 --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
+    os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p95 --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
 
