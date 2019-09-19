c0ddacbf6fbec93da974aeba0f6c728e1b5fbe07
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2019-09-19 09:02:18 +0200

    update

--- analysis/python/run/runLimitOnHephyBatch.py
+++ analysis/python/run/runLimitOnHephyBatch.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 import os
 
-data_directory              = '/afs/hephy.at/data/cms05/nanoTuples/'
-postProcessing_directory    = 'stops_2016_nano_v0p16/dilep/'
+#data_directory              = '/afs/hephy.at/data/cms05/nanoTuples/'
+#postProcessing_directory    = 'stops_2016_nano_v0p16/dilep/'
 #from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T2tt
 #from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T2bW
-from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p05 
-from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p5  
-from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p95 
+#from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p05 
+#from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p5  
+#from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p95 
 
-#data_directory              = '/afs/hephy.at/data/cms05/nanoTuples/'
-#postProcessing_directory    = 'stops_2017_nano_v0p16/dilep/'
+data_directory              = '/afs/hephy.at/data/cms05/nanoTuples/'
+postProcessing_directory    = 'stops_2017_nano_v0p16/dilep/'
 #from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T2tt
 #from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T2bW
 #from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p05 
 #from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p5  
-#from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p95 
+from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T8bbllnunu_XCha0p5_XSlep0p95 
 #
 #data_directory              = '/afs/hephy.at/data/cms05/nanoTuples/'
 #postProcessing_directory    = 'stops_2018_nano_v0p16/dilep/'
@@ -28,13 +28,13 @@ from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import sign
 #signalEstimators = [s.name for s in signals_T2tt]
 #signalEstimators = [s.name for s in signals_T2bW]
 #signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p05]
-signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p5]
-#signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p95]
+#signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p5]
+signalEstimators = [s.name for s in signals_T8bbllnunu_XCha0p5_XSlep0p95]
 
 import time
 
-cmd = "submitBatch.py --title='limT8_16'"
-#cmd = "echo"
+#cmd = "submitBatch.py --title='limT8_16'"
+cmd = "echo"
 
 print len(signalEstimators)
 for i, estimator in enumerate(signalEstimators):
@@ -44,6 +44,5 @@ for i, estimator in enumerate(signalEstimators):
     #os.system(cmd+" 'python run_limit.py --signal T2tt --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
     #os.system(cmd+" 'python run_limit.py --signal T2bW --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
     #os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p05 --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
-    os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p5 --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
-    #os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p95 --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
-
+    #os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p5 --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
+    os.system(cmd+" 'python run_limit.py --signal T8bbllnunu_XCha0p5_XSlep0p95 --expected --fitAll  --year 2016 --skipFitDiagnostics --only=%s'"%str(i))
