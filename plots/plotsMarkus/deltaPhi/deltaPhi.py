5e378078a5dd5ed3e03ef2f023dc3d1c2f5d9687
Markus Doppler <markusdoppler@me.com>
2019-10-18 11:03:59 +0200

    added delta phi cut plots

---
+++ plots/plotsMarkus/deltaPhi/deltaPhi.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+''' Lepton analysis script
+'''
+#
+# Standard imports and batch mode
+#
+import ROOT, os
+ROOT.gROOT.SetBatch(True)
+import itertools
+import copy
+import array
+import operator
+
+from math                                import sqrt, cos, sin, pi, atan2
+from RootTools.core.standard             import *
+from StopsDilepton.tools.user            import plot_directory
+from StopsDilepton.tools.helpers         import deltaPhi
+from Analysis.Tools.metFilters            import getFilterCut
+from StopsDilepton.tools.cutInterpreter  import cutInterpreter
+from StopsDilepton.tools.objectSelection import muonSelector, eleSelector, getGoodMuons, getGoodElectrons
+
+
+# JEC corrector
+from JetMET.JetCorrector.JetCorrector    import JetCorrector, correction_levels_data, correction_levels_mc
+corrector_data     = JetCorrector.fromTarBalls( [(1, 'Autumn18_RunD_V8_DATA') ], correctionLevels = correction_levels_data )
+corrector_mc       = JetCorrector.fromTarBalls( [(1, 'Autumn18_RunD_V8_DATA') ], correctionLevels = correction_levels_mc )
+
+# from Analysis.Tools.puProfileCache import *
+
+#
+# Arguments
+# 
+import argparse
+argParser = argparse.ArgumentParser(description = "Argument parser")
+argParser.add_argument('--logLevel',           action='store',      default='INFO',          nargs='?', choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET'], help="Log level for logging")
+argParser.add_argument('--signal',             action='store',      default=None,            nargs='?', choices=[None, "T2tt", "DM", "T8bbllnunu", "compilation"], help="Add signal to plot")
+argParser.add_argument('--noData',             action='store_true', default=False,           help='also plot data?')
+argParser.add_argument('--small',                                   action='store_true',     help='Run only on a small subset of the data?', )
+argParser.add_argument('--plot_directory',     action='store',      default='dPhiJet')
+argParser.add_argument('--era',                action='store', type=str,      default="Run2016")
+argParser.add_argument('--selection',          action='store',      default='lepSel-POGMetSig12-njet2p-btag0-miniIso0.2-looseLeptonMiniIsoVeto-mll20')
+argParser.add_argument('--badMuonFilters',     action='store',      default="Summer2016",  help="Which bad muon filters" )
+argParser.add_argument('--noBadPFMuonFilter',           action='store_true', default=False)
+argParser.add_argument('--noBadChargedCandidateFilter', action='store_true', default=False)
+argParser.add_argument('--unblinded',          action='store_true', default=False)
+argParser.add_argument('--blinded',            action='store_true', default=False)
+argParser.add_argument('--dpm',                action='store_true', default=False)
+argParser.add_argument('--reweightPU',         action='store', default='Central', choices=['VDown', 'Down', 'Central', 'Up', 'VUp', 'VVUp', 'noPUReweighting', 'nvtx'])
+argParser.add_argument('--isr',                action='store_true', default=False)
+args = argParser.parse_args()
+
+#
+# Logger
+#
+import StopsDilepton.tools.logger as logger
+import RootTools.core.logger as logger_rt
+logger    = logger.get_logger(   args.logLevel, logFile = None)
+logger_rt = logger_rt.get_logger(args.logLevel, logFile = None)
+
+if args.small:                        args.plot_directory += "_small"
+if args.reweightPU:                   args.plot_directory += "_%s"%args.reweightPU
+if args.noData:                       args.plot_directory += "_noData"
+if args.signal == "DM":               args.plot_directory += "_DM"
+if args.badMuonFilters!="Summer2016": args.plot_directory += "_badMuonFilters_"+args.badMuonFilters
+if args.noBadPFMuonFilter:            args.plot_directory += "_noBadPFMuonFilter"
+if args.noBadChargedCandidateFilter:  args.plot_directory += "_noBadChargedCandidateFilter"
+#
+# Make samples, will be searched for in the postProcessing directory
+#
+from Analysis.Tools.puReweighting import getReweightingFunction
+
+if "2016" in args.era:
+    year = 2016
+elif "2017" in args.era:
+    year = 2017
+elif "2018" in args.era:
+    year = 2018
+
+logger.info( "Working in year %i", year )
+
+# Load from DPM?
+if args.dpm:
+    data_directory          = "/dpm/oeaw.ac.at/home/cms/store/user/rschoefbeck/Stops2l-postprocessed/"
+
+if year == 2016:
+    from StopsDilepton.samples.nanoTuples_Summer16_postProcessed import *
+    from StopsDilepton.samples.nanoTuples_Run2016_17Jul2018_postProcessed import *
+    DY = DY_HT_LO_16
+#    mc             = [ Top_pow_16, TTXNoZ_16, TTZ_16, multiBoson_16, DY_HT_LO_16]
+elif year == 2017:
+    from StopsDilepton.samples.nanoTuples_Fall17_postProcessed import *
+    from StopsDilepton.samples.nanoTuples_Run2017_31Mar2018_postProcessed import *
+    DY = DY_HT_LO_17
+#    mc             = [ Top_pow_17, TTXNoZ_17, TTZ_17, multiBoson_17, DY_HT_LO_17]
+elif year == 2018:
+    from StopsDilepton.samples.nanoTuples_Autumn18_postProcessed import *
+    from StopsDilepton.samples.nanoTuples_Run2018_PromptReco_postProcessed import *
+    DY = DY_HT_LO_18
+#    mc             = [ Top_pow_18, TTXNoZ_18, TTZ_18, multiBoson_18, DY_HT_LO_18]
+
+# three DY's with fake MET
+DYs = [copy.deepcopy(DY) for i in range(4)]
+DYs[0].texName = "DY (E_{T, fake}^{miss} < 50)"
+DYs[0].color = ROOT.kGreen + 1
+
+DYs[1].texName = "DY (50 < E_{T, fake}^{miss} < 100)"
+DYs[1].color = ROOT.kGreen + 2
+
+DYs[2].texName = "DY (100 < E_{T, fake}^{miss} < 150)"
+DYs[2].color = ROOT.kGreen + 3
+
+DYs[3].texName = "DY (E_{T, fake}^{miss} > 150)"
+DYs[3].color = ROOT.kRed
+
+
+try:
+  data_sample = eval(args.era)
+except Exception as e:
+  logger.error( "Didn't find %s", args.era )
+  raise e
+
+lumi_scale                 = data_sample.lumi/1000
+data_sample.scale          = 1.
+for sample in DYs:
+    sample.scale          = lumi_scale
+
+if args.small:
+    for sample in DYs + [data_sample]:
+        sample.normalization = 1.
+        sample.reduceFiles( factor = 40 )
+        sample.scale /= sample.normalization
+
+#
+# Text on the plots
+#
+tex = ROOT.TLatex()
+tex.SetNDC()
+tex.SetTextSize(0.04)
+tex.SetTextAlign(11) # align right
+
+def drawObjects( plotData, dataMCScale, lumi_scale ):
+    lines = [
+      (0.15, 0.95, 'CMS Preliminary' if plotData else 'CMS Simulation'), 
+      (0.45, 0.95, 'L=%3.1f fb{}^{-1} (13 TeV) Scale %3.2f'% ( lumi_scale, dataMCScale ) ) if plotData else (0.45, 0.95, 'L=%3.1f fb{}^{-1} (13 TeV)' % lumi_scale)
+    ]
+    if "mt2ll100" in args.selection and args.noData: lines += [(0.55, 0.5, 'M_{T2}(ll) > 100 GeV')] # Manually put the mt2ll > 100 GeV label
+    return [tex.DrawLatex(*l) for l in lines] 
+
+def drawPlots(plots, mode, dataMCScale):
+  for log in [False, True]:
+    plot_directory_ = os.path.join(plot_directory, 'analysisPlots', args.plot_directory, args.era, mode + ("_log" if log else ""), args.selection)
+    for plot in plots:
+      if not max(l[0].GetMaximum() for l in plot.histos): continue # Empty plot
+      if not args.noData: 
+        if mode == "all": plot.histos[1][0].legendText = "Data"
+        if mode == "SF":  plot.histos[1][0].legendText = "Data (SF)"
+
+      _drawObjects = []
+
+      plotting.draw(plot,
+	    plot_directory = plot_directory_,
+	    ratio = {'yRange':(0.1,1.9)} if not args.noData else None,
+	    logX = False, logY = log, sorting = False,
+	    yRange = (0.03, "auto") if log else (0.001, "auto"),
+	    #scaling = {0:1},
+	    legend = ( (0.18,0.88-0.03*sum(map(len, plot.histos)),0.9,0.88), 2),
+	    drawObjects = drawObjects( not args.noData, dataMCScale , lumi_scale ) + _drawObjects,
+        copyIndexPHP = True, extensions = ["png"],
+      )
+
+#
+# Read variables and sequences
+#
+read_variables = ["weight/F", "l1_pt/F", "dl_phi/F", "dl_pt/F", "l2_pt/F", "l1_eta/F" , "l1_phi/F", "l2_eta/F", "l2_phi/F", "JetGood[pt/F,eta/F,phi/F]", "dl_mass/F", "dl_eta/F", "dl_mt2ll/F", "dl_mt2bb/F", "dl_mt2blbl/F", "met_pt/F", "met_phi/F", "MET_significance/F", "metSig/F", "ht/F", "nBTag/I", "nJetGood/I", "PV_npvsGood/I"]
+
+sequence = []
+
+def make_phi_distribution( event, sample ):
+    pass
+
+#sequence.append( make_phi_distribution )
+
+#
+#
+# default offZ for SF
+offZ = "&&abs(dl_mass-91.1876)>15" if not (args.selection.count("onZ") or args.selection.count("allZ") or args.selection.count("offZ")) else ""
+def getLeptonSelection( mode ):
+  if   mode=="mumu": return "nGoodMuons==2&&nGoodElectrons==0&&isOS&&isMuMu" + offZ
+  elif mode=="mue":  return "nGoodMuons==1&&nGoodElectrons==1&&isOS&&isEMu"
+  elif mode=="ee":   return "nGoodMuons==0&&nGoodElectrons==2&&isOS&&isEE" + offZ
+  elif mode=="SF":   return "nGoodMuons+nGoodElectrons==2&&isOS&&(isEE||isMuMu)" + offZ
+  elif mode=="all":   return "nGoodMuons+nGoodElectrons==2&&isOS&&(((isEE||isMuMu)&&" + offZ+")||isEMu)"
+
+#
+# Loop over channels
+#
+yields     = {}
+allPlots   = {}
+allModes   = ['mumu','mue','ee']
+for index, mode in enumerate(allModes):
+  yields[mode] = {}
+
+  data_sample.setSelectionString([getFilterCut(isData=True, year=year, skipBadPFMuon=args.noBadPFMuonFilter, skipBadChargedCandidate=args.noBadChargedCandidateFilter), getLeptonSelection(mode)])
+  data_sample.name           = "data"
+  data_sample.read_variables = ["event/I","run/I","reweightHEM/F"]
+  data_sample.style          = styles.errorStyle(ROOT.kBlack)
+  weight_ = lambda event, sample: event.weight*event.reweightHEM
+
+  for sample in DYs:
+    sample.read_variables = ['reweightPU/F', 'Pileup_nTrueInt/F', 'reweightDilepTrigger/F','reweightLeptonSF/F','reweightBTag_SF/F', 'reweightLeptonTrackingSF/F', 'GenMET_pt/F', 'GenMET_phi/F', "reweightHEM/F"]
+    sample.read_variables += ['reweightPU%s/F'%args.reweightPU if args.reweightPU != "Central" else "reweightPU/F"]
+    if args.reweightPU == 'Central':
+        sample.weight         = lambda event, sample: event.reweightPU*event.reweightDilepTrigger*event.reweightLeptonSF*event.reweightBTag_SF*event.reweightLeptonTrackingSF
+    else:
+        sample.weight         = lambda event, sample: getattr(event, "reweightPU"+args.reweightPU if args.reweightPU != "Central" else "reweightPU")*event.reweightDilepTrigger*event.reweightLeptonSF*event.reweightBTag_SF*event.reweightLeptonTrackingSF
+    sample.setSelectionString([getFilterCut(isData=False, year=year, skipBadPFMuon=args.noBadPFMuonFilter, skipBadChargedCandidate=args.noBadChargedCandidateFilter), getLeptonSelection(mode)])
+
+  # set selection strings to DY samples
+  DYs[0].addSelectionString('sqrt((met_pt*cos(met_phi)-GenMET_pt*cos(GenMET_phi))**2 + (met_pt*sin(met_phi)-GenMET_pt*sin(GenMET_phi))**2) < 50')
+  DYs[1].addSelectionString('sqrt((met_pt*cos(met_phi)-GenMET_pt*cos(GenMET_phi))**2 + (met_pt*sin(met_phi)-GenMET_pt*sin(GenMET_phi))**2) < 100&&sqrt((met_pt*cos(met_phi)-GenMET_pt*cos(GenMET_phi))**2 + (met_pt*sin(met_phi)-GenMET_pt*sin(GenMET_phi))**2) > 50')
+  DYs[2].addSelectionString('sqrt((met_pt*cos(met_phi)-GenMET_pt*cos(GenMET_phi))**2 + (met_pt*sin(met_phi)-GenMET_pt*sin(GenMET_phi))**2) > 100&&sqrt((met_pt*cos(met_phi)-GenMET_pt*cos(GenMET_phi))**2 + (met_pt*sin(met_phi)-GenMET_pt*sin(GenMET_phi))**2) < 150')
+  DYs[3].addSelectionString('sqrt((met_pt*cos(met_phi)-GenMET_pt*cos(GenMET_phi))**2 + (met_pt*sin(met_phi)-GenMET_pt*sin(GenMET_phi))**2) > 150')
+
+  for sample in DYs: sample.style = styles.fillStyle(sample.color)
+
+  if not args.noData:
+    stack = Stack(DYs, data_sample)
+  else:
+    stack = Stack(DYs)
+
+  # Use some defaults
+  Plot.setDefaults(stack = stack, weight = staticmethod(weight_), selectionString = cutInterpreter.cutString(args.selection), addOverFlowBin='upper', histo_class=ROOT.TH1D)
+  
+  plots = []
+
+  plots.append(Plot(
+    name = 'yield', texX = 'yield', texY = 'Number of Events',
+    attribute = lambda event, sample: 0.5 + index,
+    binning=[3, 0, 3],
+  ))
+
+#  plots.append(Plot(
+#    texX = 'miniRelIso(l_{1}) (GeV)', texY = 'Number of Events',
+#    attribute = TreeVariable.fromString( "l1_miniRelIso/F" ),
+#    binning=[30,0,0.3], addOverFlowBin = 'upper',
+#  ))
+
+# cos(met_phi-JetGood_phi)
+
+  plots.append(Plot(
+    texX = 'cos(#Delta#phi(E_{T}^{miss}, first jet))', texY = 'Number of Events',
+    name = 'dPhiJet_leading', attribute = lambda event, sample: cos(event.met_phi-event.JetGood_phi[0]),
+    binning=[200,-1.0,1.0],
+  ))
+  plots.append(Plot(
+    texX = 'cos(#Delta#phi(E_{T}^{miss}, second jet))', texY = 'Number of Events',
+    name = 'dPhiJet_subleading', attribute = lambda event, sample: cos(event.met_phi-event.JetGood_phi[1]),
+    binning=[200,-1.0,1.0],
+  ))
+
+  plotting.fill(plots, read_variables = read_variables, sequence = sequence)
+
+  # Get normalization yields from yield histogram
+  for plot in plots:
+    if plot.name == "yield":
+      for i, l in enumerate(plot.histos):
+        for j, h in enumerate(l):
+          yields[mode][plot.stack[i][j].name] = h.GetBinContent(h.FindBin(0.5+index))
+          h.GetXaxis().SetBinLabel(1, "#mu#mu")
+          h.GetXaxis().SetBinLabel(2, "e#mu")
+          h.GetXaxis().SetBinLabel(3, "ee")
+  if args.noData: yields[mode]["data"] = 0
+  yields[mode]["MC"] = sum(yields[mode][s.name] for s in DYs)
+  dataMCScale        = yields[mode]["data"]/yields[mode]["MC"] if yields[mode]["MC"] != 0 else float('nan')
+
+  drawPlots(plots, mode, dataMCScale)
+  allPlots[mode] = plots
+
+# Add the different channels into SF and all
+for mode in ["SF","all"]:
+  yields[mode] = {}
+  for y in yields[allModes[0]]:
+    try:    yields[mode][y] = sum(yields[c][y] for c in (['ee','mumu'] if mode=="SF" else ['ee','mumu','mue']))
+    except: yields[mode][y] = 0
+  dataMCScale = yields[mode]["data"]/yields[mode]["MC"] if yields[mode]["MC"] != 0 else float('nan')
+
+  for plot in allPlots['mumu']:
+    for plot2 in (p for p in (allPlots['ee'] if mode=="SF" else allPlots["mue"]) if p.name == plot.name):  #For SF add EE, second round add EMu for all
+      for i, j in enumerate(list(itertools.chain.from_iterable(plot.histos))):
+	for k, l in enumerate(list(itertools.chain.from_iterable(plot2.histos))):
+	  if i==k:
+	    j.Add(l)
+
+  drawPlots(allPlots['mumu'], mode, dataMCScale)
+
+
+logger.info( "Done with prefix %s and selectionString %s", args.selection, cutInterpreter.cutString(args.selection) )
+
