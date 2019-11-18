ae905f2e176767ab47c373bd95978e95b963944d
Markus Doppler <markusdoppler@me.com>
2019-11-18 10:50:46 +0100

    uncertainties table

--- plots/plotsMarkus/results/tableFromEstimate.py
+++ plots/plotsMarkus/results/tableFromEstimate.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from math import sqrt, cos, sin, pi
+
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("--year",                  dest="year",                  default=2016, type="int",    action="store",      help="Which year?")
@@ -22,6 +24,8 @@ import RootTools.core.logger as logger_rt
 logger_rt = logger_rt.get_logger('INFO', logFile = None )
 
 from StopsDilepton.analysis.Setup import Setup
+
+#analysis_results        = '/afs/hephy.at/data/cms05/StopsDileptonLegacy/results/v5/'
 
 setup = Setup(year=options.year)
 
@@ -69,10 +73,12 @@ estimators = estimatorList(setup)
 allEstimators = estimators.constructEstimatorList(["TTJets","TTZ","DY", 'multiBoson', 'other'])
 #allEstimators += [ MCBasedEstimate(name=s.name, sample={channel:s for channel in channels + trilepChannels}) for s in signals_T2tt ]
 
+mcEstimators = allEstimators
 for samp in signals_T2tt:
     if samp.name == "T2tt_600_0":
         s = samp
-        allEstimators += [ MCBasedEstimate(name=s.name, sample=s, cacheDir=setup.defaultCacheDir()) ]
+        signal_estimate = MCBasedEstimate(name=s.name, sample=s, cacheDir=setup.defaultCacheDir())
+        allEstimators += [ signal_estimate ]
 
 
 setup.verbose=True
@@ -102,40 +108,44 @@ systematic_uncertainties_list = [\
 #    Region('MET_significance', (12, 50))+Region('dl_mt2blbl', (100, 200))+Region('dl_mt2ll', (140, 240)),
 ##    Region('MET_significance', (12, 50))+Region('dl_mt2blbl', (200, -1))+Region('dl_mt2ll', (140, 240)),
 #]
-allRegions = [Region('MET_significance', (12, 50))+Region('dl_mt2blbl', (100, 200))+Region('dl_mt2ll', (140, 240))]
-#allRegions
+#allRegions = [Region('MET_significance', (12, 50))+Region('dl_mt2blbl', (100, 200))+Region('dl_mt2ll', (140, 240))]
+#allRegions = allRegions[1:]
+allRegions
 
-channels = ['all']#['SF','all']
+channels = ['SF','EMu']
 
 sys_errors = {}
-up_variation = {}
-down_variation = {}
-reference = {}
+up_cachedEstimate = {}
+down_cachedEstimate = {}
+ref_cachedEstimate = {}
+sys_cachedEstimate = {}
 
 e_yield = {}
 for estimate in allEstimators:
     # Select estimate
-    if estimate.name is not 'Data':
+    if "T2tt" in estimate.name:
+        setup = setup.sysClone(sys={'reweight':['reweight_nISR', 'reweightLeptonFastSimSF'], 'remove':[]}) 
+        e = estimate
+        #assert False, "Signal"
+        #e.cachedEstimate(r, channel, signalSetup)
+        e.isSignal = True
+        e.isData   = False
+    elif estimate.name is not 'Data':
         e = estimate
         e.isSignal = False
         e.isData = False
-    elif "T2tt" in estimate.name:
-        setup = setup.sysClone(sys={'reweight':['reweight_nISR', 'reweightLeptonFastSimSF'], 'remove':[]}) 
-        e = estimate
-        assert False, "Signal"
-        #e.cachedEstimate(r, channel, signalSetup)
-        e.isData   = False
     else:
         assert False, "Cannot look at data!! We're blinded!!"
         estimate = DataObservation(name='Data', sample=setup.samples['Data'], cacheDir=setup.defaultCacheDir())
         e.isSignal = False
         e.isData   = True
     print e.name
-    sys_errors[e.name] = {}
-    up_variation[e.name] = {}
-    down_variation[e.name] = {}
-    reference[e.name] = {}
     e.initCache(setup.defaultCacheDir())
+    sys_errors[e.name] = {}
+    up_cachedEstimate[e.name] = {}
+    down_cachedEstimate[e.name] = {}
+    ref_cachedEstimate[e.name] = {}
+    sys_cachedEstimate[e.name] = {}
 
 #    if e.name.count('T2tt') or e.name.count('TTbarDM') or e.name.count('T8bbllnunu'): e.isSignal = True
 #
@@ -145,136 +155,208 @@ for estimate in allEstimators:
 #      setup = setup.sysClone(sys={'reweight':['reweightLeptonFastSimSF'], 'remove':['reweightPU36fb']})
 
     for channel in channels:
-        up_variation[e.name][channel] = []
-        down_variation[e.name][channel] = []
-        reference[e.name][channel] = []
+        up_cachedEstimate[e.name][channel] = []
+        down_cachedEstimate[e.name][channel] = []
+        ref_cachedEstimate[e.name][channel] = []
+        sys_cachedEstimate[e.name][channel] = []
         for (i, r) in enumerate(allRegions):
-            up_variation[e.name][channel].append({})
-            down_variation[e.name][channel].append({})
-            reference[e.name][channel].append({})
-            #print " ", i, r
+            up_cachedEstimate[e.name][channel].append({})
+            down_cachedEstimate[e.name][channel].append({})
+            ref_cachedEstimate[e.name][channel].append({})
+            sys_cachedEstimate[e.name][channel].append({})
+            print " Region", i, "|", channel
             e_yield[e.name] = e.cachedEstimate(r, channel, setup).val
             for syst in systematic_uncertainties_list:
+                #print "\t"+syst
                 sys_contribution = eval("e."+syst+"Systematic")(r, channel, setup)
-                #print sys_contribution.sigma
-                #print "    "+syst+":", "{:.2f} %".format(sys_contribution.sigma/total_val*100) if total_val != 0 else float('nan')
+                ref  = e.cachedEstimate(r, channel, setup)
+
+                #! getting systematics with functions from SystematicEstimator.py
                 if syst == "PU":
-                    ref  = e.cachedEstimate(r, channel, setup)
-                    up   = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightPUUp']}))
-                    down = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightPUDown']}))
-                    #print "\t\t\tref: {} up: {} down: {}".format(ref.val,up.val,down.val)
+                    if options.year == 2018:
+                        up   = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightPUVVUp']}))
+                        down = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightPUUp']}))
+                    else: 
+                        up   = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightPUUp']}))
+                        down = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightPUDown']}))
+                    sys = e.PUSystematic(r, channel, setup).val
                 elif syst == "JER":
-                    ref  = e.cachedEstimate(r, channel, setup)
                     up   = e.cachedEstimate(r, channel, setup.sysClone({'selectionModifier':'jerUp'}))
                     down = e.cachedEstimate(r, channel, setup.sysClone({'selectionModifier':'jerDown'}))
-                    #print "\t\t\tref: {} up: {} down: {}".format(ref.val,up.val,down.val)
+                    syst_tuple = e.JERSystematicAsym(r, channel, setup)
+                    #print "\t\t", abs(syst_tuple[0]-1), "|", abs(syst_tuple[1]-1)
+                    sys = max(abs(syst_tuple[0]-1), abs(syst_tuple[1]-1)) if syst_tuple[0] != 0. or syst_tuple[1] != 0. else 0.
                 elif syst == "JEC":
-                    ref  = e.cachedEstimate(r, channel, setup)
                     up   = e.cachedEstimate(r, channel, setup.sysClone({'selectionModifier':'jesTotalUp'}))
                     down = e.cachedEstimate(r, channel, setup.sysClone({'selectionModifier':'jesTotalDown'}))
-                    #print "\t\t\tref: {} up: {} down: {}".format(ref.val,up.val,down.val)
+                    syst_tuple = e.JECSystematicAsym(r, channel, setup)
+                    #print "\t\t", abs(syst_tuple[0]-1), "|", abs(syst_tuple[1]-1)
+                    sys = max(abs(syst_tuple[0]-1), abs(syst_tuple[1]-1)) if syst_tuple[0] != 0. or syst_tuple[1] != 0. else 0.
                 elif syst == "topPt":
-                    ref  = e.cachedEstimate(r, channel, setup)
                     up   = e.cachedEstimate(r, channel, setup.sysClone({'remove':['reweightTopPt']}))
                     down = ref
-                    #print "\t\t\tref: {} up: {}".format(ref.val,up.val)
+                    sys = e.topPtSystematic(r, channel, setup).val
                 elif syst == "unclustered":
-                    ref  = e.cachedEstimate(r, channel, setup)
                     up   = e.cachedEstimate(r, channel, setup.sysClone({'selectionModifier':'unclustEnUp'}))
                     down = e.cachedEstimate(r, channel, setup.sysClone({'selectionModifier':'unclustEnDown'}))
-                    #print "\t\t\tref: {} up: {} down: {}".format(ref.val,up.val,down.val)
+                    syst_tuple = e.unclusteredSystematicAsym(r, channel, setup)
+                    #print "\t\t", abs(syst_tuple[0]-1), "|", abs(syst_tuple[1]-1)
+                    sys = max(abs(syst_tuple[0]-1), abs(syst_tuple[1]-1)) if syst_tuple[0] != 0. or syst_tuple[1] != 0. else 0.
                 elif syst == "btaggingSFb":
-                    ref  = e.cachedEstimate(r, channel, setup)
                     up   = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightBTag_SF_b_Up']}))
                     down = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightBTag_SF_b_Down']}))
-                    #print "\t\t\tref: {} up: {} down: {}".format(ref.val,up.val,down.val)
+                    sys = e.btaggingSFbSystematic(r, channel, setup).val
                 elif syst == "btaggingSFl":
-                    ref  = e.cachedEstimate(r, channel, setup)
                     up   = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightBTag_SF_l_Up']}))
                     down = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightBTag_SF_l_Down']}))
-                    #print "\t\t\tref: {} up: {} down: {}".format(ref.val,up.val,down.val)
+                    sys = e.btaggingSFlSystematic(r, channel, setup).val
                 elif syst == "leptonSF":
-                    ref  = e.cachedEstimate(r, channel, setup)
                     up   = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightLeptonSFUp']}))
                     down = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightLeptonSFDown']}))
-                    #print "\t\t\tref: {} up: {} down: {}".format(ref.val,up.val,down.val)
+                    sys = e.leptonSFSystematic(r, channel, setup).val
                 elif syst == "trigger":
-                    ref  = e.cachedEstimate(r, channel, setup)
                     up   = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightDilepTriggerUp']}))
                     down = e.cachedEstimate(r, channel, setup.sysClone({'reweight':['reweightDilepTriggerDown']}))
-                    #print "\t\t\tref: {} up: {} down: {}".format(ref.val,up.val,down.val)
+                    sys = e.triggerSystematic(r, channel, setup).val
                 
-                up_variation[e.name][channel][i][syst] = up
-                down_variation[e.name][channel][i][syst] = down
-                reference[e.name][channel][i][syst] = ref 
+                #print "\t\tref {} | up {} | down {}".format(ref.val,up.val,down.val)
+                #print "\t\t", sys, "(systematicEstimator)"
+                #print "\t\t", (up.val-down.val)/(2*ref.val) if ref.val>0 else 0, "(self-made)"
 
-                #print "\t"+syst+":", "{:.3f} ({:.3f})".format(sys_contribution.val, e_yield[e.name])
+                up_cachedEstimate[e.name][channel][i][syst] = up
+                down_cachedEstimate[e.name][channel][i][syst] = down
+                ref_cachedEstimate[e.name][channel][i][syst] = ref 
+                sys_cachedEstimate[e.name][channel][i][syst] = sys
                 sys_errors[e.name][syst] = sys_contribution.val
 
 
 # OUTPUT
 
-header_string = "\t"
-est_yield = 0
-for e in allEstimators:
-    if not "T2tt" in e.name: est_yield += e_yield[e.name]
-    header_string += "\t&" + e.name
-print est_yield
-print header_string + " \\\\ \\hline\\hline"
-#print "&", "&".join(systematic_uncertainties_list), "& yield\\\\ \\hline"
-
-for syst in systematic_uncertainties_list:
-    est_string = syst+"\t"
-    for e in allEstimators:
-        #est_string += "\t& {:.1f}\\%".format(sys_errors[e.name][syst]/est_yield*100 if est_yield != 0 else float('nan')) 
-        est_string += "\t& {:.1f}\\%".format(sys_errors[e.name][syst]*100 if est_yield != 0 else float('nan')) 
-    print est_string + "\\\\"
-
-est_string = "\\hline\n"+"yield = ${:.1f}$\t".format(est_yield)
-for e in allEstimators:
-    est_string += "\t& {:.1f}".format(e_yield[e.name]) 
-print est_string + "\\\\"
+#header_string = "\t"
+#est_yield = 0
+#for e in allEstimators:
+#    if not "T2tt" in e.name: est_yield += e_yield[e.name]
+#    header_string += "\t&" + e.name
+##print est_yield
+##print header_string + " \\\\ \\hline\\hline"
+##print "&", "&".join(systematic_uncertainties_list), "& yield\\\\ \\hline"
+#
+#for syst in systematic_uncertainties_list:
+#    est_string = syst+"\t"
+#    for e in allEstimators:
+#        #est_string += "\t& {:.1f}\\%".format(sys_errors[e.name][syst]/est_yield*100 if est_yield != 0 else float('nan')) 
+#        est_string += "\t& {:.1f}\\%".format(sys_errors[e.name][syst]*100 if est_yield != 0 else float('nan')) 
+#    #print est_string + "\\\\"
+#
+#est_string = "\\hline\n"+"yield = ${:.1f}$\t".format(est_yield)
+#for e in allEstimators:
+#    est_string += "\t& {:.1f}".format(e_yield[e.name]) 
+##print est_string + "\\\\"
 
 
 
 # OUTPUT 2
 
-header_string = "        "
+header_string = "                "
 
-
+export_string = ""
+unc_string = ""
 for syst in systematic_uncertainties_list:
+    print syst
     slide_start = """
     \\begin{frame}
-        \\frametitle{"""+syst+"""}
+        \\frametitle{"""+syst+" | "+str(options.year)+"""}
         \\begin{table}[h!]
             \\renewcommand{\\arraystretch}{1.5}
             \\centering
             \\footnotesize
-            \\begin{tabular}{r||c|c|c|c|c||c}
-    """
+            \\begin{tabular}{r||"""+"||".join(["c|c" for channel in channels])+"}\n"
+    table_header = header_string
+    for channel in channels:
+        table_header += "& \\textbf{MC "+channel+" channel}"
+        table_header += "& \\textbf{T2tt 600 0 "+channel+" channel}"
+    table_header += "\\\\ \\hline\\hline\n"
     slide_end = """
             \\end{tabular}
         \\end{table}
     \\end{frame}
     """
-    print syst
+
     table_data=""
+    min_unc_mc = 999.0
+    max_unc_mc = 0.0
+    min_unc_sig = 999.0
+    max_unc_sig = 0.0
     for (i, r) in enumerate(allRegions):
-        print "  Region",i
-        for channel in channels:
-            print "    "+channel+" channel"
+        #print "  Region",i
+
+        table_row = header_string+"Region "+str(i)
+
+        for (i_c, channel) in enumerate(channels):
+            #print "    "+channel+" channel"
             up_est = 0
             down_est = 0
             ref_est = 0
-            for estimate in allEstimators:
-                up_est += up_variation[estimate.name][channel][i][syst]
-                down_est += down_variation[estimate.name][channel][i][syst]
-                ref_est += reference[estimate.name][channel][i][syst]
-            print "      up:   ", up_est.val
-            print "      ref:  ", ref_est.val
-            print "      down: ", down_est.val
-            print "      up-ref:  ", (up_est.val-ref_est.val)/ref_est.val if ref_est.val != 0 else 0 
-            print "      ref-down:", (up_est.val-ref_est.val)/ref_est.val if ref_est.val != 0 else 0 
-            table_data += ""
+            for estimate in mcEstimators:
+                up_est   += up_cachedEstimate[estimate.name][channel][i][syst]
+                down_est += down_cachedEstimate[estimate.name][channel][i][syst]
+                ref_est  += ref_cachedEstimate[estimate.name][channel][i][syst]
+            
+            #! adding absolute uncertainties in quadrature
+            sys_est = 0
+            for estimate in mcEstimators:
+                sys_est  += (sys_cachedEstimate[estimate.name][channel][i][syst]*ref_cachedEstimate[estimate.name][channel][i][syst].val)**2
 
-    #print slide_start+table_data+slide_end+"\n\n\n"
+            if ref_est.val == 0.: print "Region", i, "| Channel", channel, ": ref yield is zero"
+            mc_uncertainty = sqrt(sys_est)/ref_est.val if ref_est.val != 0 else 0.
+            signal_uncertainty = sys_cachedEstimate[signal_estimate.name][channel][i][syst]
+            #print "mc    ", mc_uncertainty
+            #print "signal", signal_uncertainty
+
+            table_row += " & {:.1f}\\% & {:.1f}\\%".format(abs(mc_uncertainty)*100, abs(signal_uncertainty)*100)
+            #print "\t\t\t{:.1f}\\%".format(abs(mc_uncertainty)*100)
+            #print "\t\t\t{:.1f}\\%".format(abs(signal_uncertainty)*100)
+
+            # max/minimasation of uncertainties
+            if mc_uncertainty < min_unc_mc: min_unc_mc = mc_uncertainty
+            if mc_uncertainty > max_unc_mc: max_unc_mc = mc_uncertainty
+            if signal_uncertainty < min_unc_sig: min_unc_sig = signal_uncertainty
+            if signal_uncertainty > max_unc_sig: max_unc_sig = signal_uncertainty
+         
+        table_data += table_row + "\\\\ \n"
+
+    print "\t\t{:.1f} - {:.1f}\\%".format(abs(min_unc_mc*100), abs(max_unc_mc*100))
+    #print " & {:.1f}-{:.1f}\\%".format(abs(min_unc_sig*100), abs(max_unc_sig*100))
+    unc_string += "{} & {:.1f}-{:.1f}\\% & {:.1f}-{:.1f}\\% \\ \n".format(syst, min_unc_mc*100, max_unc_mc*100, min_unc_sig*100, max_unc_sig*100)    
+
+    export_string += slide_start+table_header+table_data+slide_end+"\n\n\n"
+
+#print export_string
+#print unc_string
+
+export_path = "/afs/hephy.at/user/m/mdoppler/www/uncertaintiesTable/"
+with file( export_path + "table_v5_"+str(options.year)+".tex", 'w' ) as f:
+    f.write( export_string )
+    #f.write( "\n\n"+unc_string )
+
+
+for (i, r) in enumerate(allRegions):
+    print "Region", i, "-", r
+
+
+# OUTPUT 3
+
+for (i, r) in enumerate(allRegions):
+    for (i_c, channel) in enumerate(channels):
+        yield_sum = 0
+        for estimate in mcEstimators:
+            yield_sum += ref_cachedEstimate[estimate.name][channel][i][syst].val
+
+        #! adding absolute uncertainties in quadrature
+        unc_sum = 0
+        for estimate in mcEstimators:
+            for syst in systematic_uncertainties_list:
+                unc_sum += (sys_cachedEstimate[estimate.name][channel][i][syst]*ref_cachedEstimate[estimate.name][channel][i][syst].val)**2
+
+        print "Region", i, "|\t", channel, "\t{:3.1f}% ({:.1f})".format(100*sqrt(unc_sum)/yield_sum if yield_sum != 0 else 0., yield_sum)
+
