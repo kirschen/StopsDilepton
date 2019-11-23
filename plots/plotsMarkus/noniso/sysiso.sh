25295e8bce45d86cdca2db992d8a9008caa09ddb
Markus Doppler <markusdoppler@me.com>
2019-11-23 21:20:04 +0100

    v0p19

--- plots/plotsMarkus/noniso/sysiso.sh
+++ plots/plotsMarkus/noniso/sysiso.sh
@@ -1,9 +1,9 @@
 # calculate uncertainty histos
-#python sysiso.py --dpm --era Run2016 --reweightPU Central --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1 --overwrite
-#python sysiso.py --dpm --era Run2017 --reweightPU Central --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1-badEEJetVeto --overwrite
-#python sysiso.py --dpm --era Run2018 --reweightPU VUp --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1 --overwrite
+python sysiso.py --dpm --era Run2016 --plot_directory v0p19 --reweightPU Central --normalized --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1 #--overwrite
+python sysiso.py --dpm --era Run2017 --plot_directory v0p19 --reweightPU Central --normalized --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1-badEEJetVeto --add #--overwrite
+python sysiso.py --dpm --era Run2018 --plot_directory v0p19 --reweightPU VUp --normalized --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1 --add #--overwrite
 
 # plot
-python sysiso.py --dpm --era Run2016 --reweightPU Central --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1 --normalize
-python sysiso.py --dpm --era Run2017 --reweightPU Central --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1-badEEJetVeto --normalize
-python sysiso.py --dpm --era Run2018 --reweightPU VUp --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1 --normalize
+#python sysiso.py --dpm --era Run2016 --plot_directory v0p19 --reweightPU Central --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1 --normalize
+#python sysiso.py --dpm --era Run2017 --plot_directory v0p19 --reweightPU Central --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1-badEEJetVeto --normalize
+#python sysiso.py --dpm --era Run2018 --plot_directory v0p19 --reweightPU VUp --selection lepSel1Tight-njet4p-btag1p-dPhiJet0-dPhiJet1 --normalize
