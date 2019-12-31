822a71d84ad2ef86bca190f05c4cc50bb6eae730
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2019-12-31 11:16:30 +0100

    running remaining signal scale uncertainties

--- analysis/python/run/runPDFandScale.py
+++ analysis/python/run/runPDFandScale.py
@@ -92,6 +92,21 @@ if options.signal:
             postProcessing_directory    = 'stops_2018_nano_v0p21/dilep/'
             from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T2tt as jobs
 
+    elif options.signal == 'T2bW':
+        if year == 2016:
+            data_directory              = '/afs/hephy.at/data/cms02/nanoTuples/'
+            postProcessing_directory    = 'stops_2016_nano_v0p19/dilep/'
+            from StopsDilepton.samples.nanoTuples_FastSim_Summer16_postProcessed import signals_T2bW as jobs
+        elif year == 2017:
+            data_directory              = '/afs/hephy.at/data/cms01/nanoTuples/'
+            postProcessing_directory    = 'stops_2017_nano_v0p19/dilep/'
+            from StopsDilepton.samples.nanoTuples_FastSim_Fall17_postProcessed import signals_T2bW as jobs
+        if year == 2018:
+            data_directory              = '/afs/hephy.at/data/cms02/nanoTuples/'
+            postProcessing_directory    = 'stops_2018_nano_v0p19/dilep/'
+            from StopsDilepton.samples.nanoTuples_FastSim_Autumn18_postProcessed import signals_T2bW as jobs
+
+
     if options.only.isdigit():
         sample = jobs[int(options.only)]
     else:
