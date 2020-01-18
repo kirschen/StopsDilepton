b16cf652c17ba96ce385b6fc09fab96c3f39efbe
Markus Doppler <markusdoppler@me.com>
2020-01-18 14:22:14 +0100

    SR plots

--- plots/plotsMarkus/deltaPhi/deltaPhi.sh
+++ plots/plotsMarkus/deltaPhi/deltaPhi.sh
@@ -1,3 +1,3 @@
-python deltaPhi.py --era Run2016 --reweightPU Central --plot_directory v0p19_dPhiJet --noData --selection lepSel-POGMetSig12-njet2p-btag0-miniIso0.2-looseLeptonMiniIsoVeto-mll20
-python deltaPhi.py --era Run2017 --reweightPU Central --plot_directory v0p19_dPhiJet --noData --selection lepSel-badEEJetVeto-POGMetSig12-njet2p-btag0-miniIso0.2-looseLeptonMiniIsoVeto-mll20
-python deltaPhi.py --era Run2018 --reweightPU VUp --plot_directory v0p19_dPhiJet --noData --selection lepSel-POGMetSig12-njet2p-btag0-miniIso0.2-looseLeptonMiniIsoVeto-mll20
+python deltaPhi.py --era Run2016 --reweightPU Central --plot_directory v0p19_dPhiJet --noData --selection lepSel-POGMetSig12-njet2p-btag1p-miniIso0.2-looseLeptonMiniIsoVeto-mll20
+python deltaPhi.py --era Run2017 --reweightPU Central --plot_directory v0p19_dPhiJet --noData --selection lepSel-badEEJetVeto-POGMetSig12-njet2p-btag1p-miniIso0.2-looseLeptonMiniIsoVeto-mll20
+python deltaPhi.py --era Run2018 --reweightPU VUp --plot_directory v0p19_dPhiJet --noData --selection lepSel-POGMetSig12-njet2p-btag1p-miniIso0.2-looseLeptonMiniIsoVeto-mll20
